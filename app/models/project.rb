995dcabbda09a32dbd960b852b8905a7d6efe768
Satohi Tahara <tahara@digital-squad.net>
2013-07-09 12:29:35 +0900

    added scopes for Project to order by relations count

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :docs
+  has_and_belongs_to_many :pmdocs, :join_table => :docs_projects, :class_name => 'Doc', :conditions => {:sourcedb => 'PubMed'}
+  has_and_belongs_to_many :pmcdocs, :join_table => :docs_projects, :class_name => 'Doc', :conditions => {:sourcedb => 'PMC'}
 
   attr_accessible :name, :description, :author, :license, :status, :accessibility, :reference, :viewer, :editor, :rdfwriter, :xmlwriter, :bionlpwriter
   has_many :denotations, :dependent => :destroy
@@ -9,4 +11,34 @@ class Project < ActiveRecord::Base
   has_many :modifications, :dependent => :destroy
 
   validates :name, :presence => true, :length => {:minimum => 5, :maximum => 30}
+  
+  scope :accessible, lambda{|current_user|
+    if current_user.present?
+      where(:user_id => current_user.id)
+    else
+      where(:accessibility => 1)
+    end
+  }
+    
+  # scopes for order
+  scope :order_pmdocs_count, 
+    includes(:pmdocs).
+    group('projects.id').
+    order("IFNULL(count(docs.id), 0) DESC")
+    
+  scope :order_pmcdocs_count, 
+    includes(:pmcdocs).
+    group('projects.id').
+    order("IFNULL(count(docs.id), 0) DESC")
+    
+  scope :order_denotations_count, 
+    includes(:denotations).
+    group('projects.id').
+    order("IFNULL(count(denotations.id), 0) DESC")
+    
+  scope :order_relations_count,
+    #joins('LEFT OUTER JOIN relations ON relations.project_id = projects.id').
+    includes(:relations).
+    group('projects.id').
+    order('IFNULL(count(relations.id), 0) DESC')
 end
