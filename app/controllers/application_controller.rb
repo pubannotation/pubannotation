7578ac98f8957cf2deb82236d1229643dd96f348
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2013-08-05 11:45:29 +0900

    aligner renamed to sequence_alignment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 require 'pmcdoc'
 require 'utfrewrite'
-require 'aligner'
+require 'sequence_alignment'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
