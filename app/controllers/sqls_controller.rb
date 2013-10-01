ae9f85012176e5b630f5091fcfccaa8dc3fa5212
Satohi Tahara <tahara@digital-squad.net>
2013-10-01 15:50:29 +0900

    Added pagination

--- app/controllers/sqls_controller.rb
+++ app/controllers/sqls_controller.rb
@@ -2,9 +2,13 @@ class SqlsController < ApplicationController
   require 'json'
   
   def index
+    # TODO
+    # filter => limit users who can execute this action
+    # limit commands => ex: DROP TABLE, DELETE, UPDATE
     if params[:sql].present?
       sanitized_sql = ActiveRecord::Base::sanitize(params[:sql]).gsub('\'', '')
       @results = ActiveRecord::Base.connection.execute(sanitized_sql)
+      @results = @results.paginate(:page => params[:page])
     end
   end
 end
