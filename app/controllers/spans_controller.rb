cd510a510f07c83e470791c4e36588c940ddb0a1
Satohi Tahara <tahara@digital-squad.net>
2013-10-02 16:06:31 +0900

    Added doc.sourcedb,sourceid,doc_id and spans to spans#sql view.

--- app/controllers/spans_controller.rb
+++ app/controllers/spans_controller.rb
@@ -4,6 +4,8 @@ class SpansController < ApplicationController
       sanitized_sql = ActiveRecord::Base::sanitize(params[:sql]).gsub('\'', '')
       @results = Denotation.connection.execute(sanitized_sql)
       @results = @results.paginate(:page => params[:page], :per_page => 50)
+      @doc_ids = @results.collect{|result| result['doc_id']}.uniq
+      @docs = Doc.find(@doc_ids)
     end
   end
 end
