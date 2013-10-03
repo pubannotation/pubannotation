e5f7d73a91c3fa05699ced8175c9a1568420ae4b
Satohi Tahara <tahara@digital-squad.net>
2013-10-03 11:22:05 +0900

    Replaced raw sql results to ActiveRecort object

--- app/controllers/spans_controller.rb
+++ app/controllers/spans_controller.rb
@@ -3,9 +3,8 @@ class SpansController < ApplicationController
     if params[:sql].present?
       sanitized_sql = ActiveRecord::Base::sanitize(params[:sql]).gsub('\'', '')
       @results = Denotation.connection.execute(sanitized_sql)
-      @results = @results.paginate(:page => params[:page], :per_page => 50)
-      @doc_ids = @results.collect{|result| result['doc_id']}.uniq
-      @docs = Doc.find(@doc_ids)
+      @ids = @results.collect{| result | result['id']}
+      @denotations = Denotation.where('id IN(?)', @ids).includes(:doc).order('id ASC').paginate(:page => params[:page], :per_page => 50)
     end
   end
 end
