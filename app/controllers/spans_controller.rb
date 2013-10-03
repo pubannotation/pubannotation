beb668b56bdbb0c6f01777d5413f1488b28d1a62
Satohi Tahara <tahara@digital-squad.net>
2013-10-03 16:35:58 +0900

    Added project scope for spans#sql

--- app/controllers/spans_controller.rb
+++ app/controllers/spans_controller.rb
@@ -1,10 +1,17 @@
 class SpansController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index, :show, :autocomplete_pmcdoc_sourceid, :autocomplete_pmdoc_sourceid, :search]
+  include ApplicationHelper
+  
   def sql
     if params[:sql].present?
-      sanitized_sql = ActiveRecord::Base::sanitize(params[:sql]).gsub('\'\'', '"').gsub('\'', '')
+      accessible_project_ids = Project.accessible(current_user).collect{|project| project.id}
+      sanitized_sql =  sanitize_sql(params[:sql])
       @results = Denotation.connection.execute(sanitized_sql)
       @ids = @results.collect{| result | result['id']}
-      @denotations = Denotation.where('id IN(?)', @ids).includes(:doc).order('id ASC').paginate(:page => params[:page], :per_page => 50)
+      @denotations = Denotation.
+        where('project_id IN(?)', accessible_project_ids).
+        where('id IN(?)', @ids).
+        includes(:doc).order('id ASC').paginate(:page => params[:page], :per_page => 50)
     end
   end
 end
