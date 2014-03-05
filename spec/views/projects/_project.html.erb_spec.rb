d2ee86471cf486ab07afa2e13a94af85ee06fdfc
Mac mini <tahara@digital-squad.net>
2014-03-05 20:29:50 +0900

    Fixed spec for projects/_projects

--- spec/views/projects/_project.html.erb_spec.rb
+++ spec/views/projects/_project.html.erb_spec.rb
@@ -8,29 +8,5 @@ describe "projects/_project.html.erb" do
       view.stub(:user_signed_in?).and_return(false)
       view.stub(:current_user).and_return(nil)
     end
-
-    context 'when @accordion_id present' do
-      before do
-        @accordion_id = 'accordion'
-        assign :accordin_id, @accordion_id
-        view.stub(:model).and_return(@project)
-        render
-      end
-      
-      it 'should render active id' do
-        view.content_for(:javascript).should include "active: #{@accordion_id}"
-      end 
-    end
-
-    context 'when @accordion_id present' do
-      before do
-        view.stub(:model).and_return(@project)
-        render
-      end
-      
-      it 'should not render active id' do
-        view.content_for(:javascript).should_not include "active:"
-      end 
-    end
   end
 end
