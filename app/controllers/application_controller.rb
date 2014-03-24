d69e764be3f58b9d97c57cbb4a3d0743fa4d6512
Jin-Dong Kim <jindong.kim@gmail.com>
2014-03-25 00:57:04 +0900

    revised to work with the new API of pubdictionaries.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -175,7 +175,7 @@ class ApplicationController < ActionController::Base
 
 
   def gen_annotations (annotations, annserver, options = nil)
-    RestClient.post annserver, {:text => annotations[:text].to_json, :options => {}.to_json}, :content_type => :json, :accept => :json do |response, request, result|
+    RestClient.post annserver, {:text => annotations[:text], :options => {}.to_json}, :content_type => :json, :accept => :json do |response, request, result|
       case response.code
       when 200
         denotations = JSON.parse response, :symbolize_names => true
