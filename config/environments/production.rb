ab2168514c511345b2f1025b2510ffb456171a36
Mac mini <tahara@digital-squad.net>
2014-06-11 17:25:16 +0900

    Set sample setting values to action mailer and devise mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Pubann::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # mail delivery_method
+  config.action_mailer.delivery_method = :smtp
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
---
+++ config/initializers/mailer.rb
@@ -0,0 +1,7 @@
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.pubannotation.org',
+  :port => 587,
+  :domain => 'pubannotation.org',
+  :user_name => 'mail user name',
+  :password => 'password'
+}
