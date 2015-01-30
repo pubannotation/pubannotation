d4bebc41313f9ed0bf65d4122ede0c227ac65902
Mac mini <tahara@digital-squad.net>
2015-01-30 17:25:58 +0900

    Revised projects#index link

--- spec/views/projects/_list.html.erb_spec.rb
+++ spec/views/projects/_list.html.erb_spec.rb
@@ -24,6 +24,87 @@ describe "projects/_list.html.erb" do
     end
   end
 
+  describe 'index link' do
+    before do
+      view.stub(:scope).and_return('user_projects')
+    end
+
+    context 'when params[:controller] == projects' do
+      before do
+        @params = {controller: 'projects'}
+      end
+
+      context 'when params[:action] == index' do
+        before do
+          @params[:action] = 'index'
+        end
+
+        context 'when params[:sort_direction] present' do
+          before do
+            @params[:sort_direction] = 'ASC'
+            view.stub(:params).and_return(@params)
+            render 
+          end
+
+          it 'should include projects_path link' do
+            expect(rendered).to have_selector(:a, href: projects_path)
+          end
+        end
+
+        context 'when params[:sort_direction] nil' do
+          before do
+            view.stub(:params).and_return(@params)
+            render 
+          end
+
+          it 'should not include projects_path link' do
+            expect(rendered).not_to have_selector(:a, href: projects_path)
+          end
+        end
+      end
+
+      context 'when params[:action] != index' do
+        before do
+          @params[:action] = 'show'
+          view.stub(:params).and_return(@params)
+          render 
+        end
+
+        it 'should include projects_path link' do
+          expect(rendered).to have_selector(:a, href: projects_path)
+        end
+      end
+    end
+
+    context 'when params[:controller] != projects' do
+      before do
+        @params = {controller: 'home'}
+        view.stub(:params).and_return(@params)
+      end
+
+      context 'whene @doc blank' do
+        before do
+          render 
+        end
+
+        it 'should include projects_path link' do
+          expect(rendered).to have_selector(:a, href: projects_path)
+        end
+      end
+
+      context 'whene @doc present' do
+        before do
+          assign :doc, FactoryGirl.create(:doc)
+          render 
+        end
+
+        it 'should include projects_path link' do
+          expect(rendered).not_to have_selector(:a, href: projects_path)
+        end
+      end
+    end
+  end
+
   describe 'counter' do
     before do
       assign :projects, [@project]
