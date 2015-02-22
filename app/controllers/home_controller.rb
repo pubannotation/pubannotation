db1dcc1edc2455173b240f4f93e9ef16c6b583dd
Jin-Dong Kim <jindong.kim@gmail.com>
2015-02-23 05:54:58 +0900

    links to doc list are removed from the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     # @sourcedbs = Doc.select(:sourcedb).sourcedbs.uniq
     @sourcedb_doc_counts = Doc.where("serial = ?", 0).group(:sourcedb).count
+    # @docs_number = Doc.where("serial = ?", 0).count
     @projects_number = Project.accessible(current_user).length
     @projects_top = Project.accessible(current_user).top
   end
