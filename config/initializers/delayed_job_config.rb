5a8c5bde6bcf56d7bea134c9d42538a596bf4df5
Jin-Dong Kim <jindong.kim@gmail.com>
2015-11-30 17:37:08 +0900

    REVISED worker run time is adjusted.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,4 +1,4 @@
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.max_run_time = 7.days
+Delayed::Worker.max_run_time = 14.days
