3c38bb64c494234decf97bdad4e72fd30b593db9
Jin-Dong Kim <jindong.kim@gmail.com>
2016-01-20 01:04:37 +0900

    FIXED exist? is much faster than present?

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -205,7 +205,7 @@ class ProjectsController < ApplicationController
       project = Project.editable(current_user).find_by_name(params[:project_id])
       raise "There is no such project in your management." unless project.present?
 
-      message = if project.docs.present?
+      message = if project.docs.exists?
         priority = project.jobs.unfinished.count
         delayed_job = Delayed::Job.enqueue DeleteAllDocsFromProjectJob.new(project, current_user), priority: priority, queue: :general
         Job.create({name:'Delete all documents from project', project_id:project.id, delayed_job_id:delayed_job.id})
