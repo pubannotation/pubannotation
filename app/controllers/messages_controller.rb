ed6bcfba2c978318f9d2510952d537b3b45e5f48
Jin-Dong Kim <jindong.kim@gmail.com>
2016-01-21 17:33:21 +0900

    FIX messages order

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,17 @@
 class MessagesController < ApplicationController
-  before_filter :authenticate_user!
+	before_filter :authenticate_user!
 
-  def index
+	def index
 		@project = Project.editable(current_user).find_by_name(params[:project_id])
-    raise "There is no such project in your management." unless @project.present?
+		raise "There is no such project in your management." unless @project.present?
 
-    @job = Job.find(params[:id])
-		@messages = Message.where("job_id = ?", @job)
+		@job = Job.find(params[:id])
+		@messages = @job.messages.order(:created_at)
 
-    respond_to do |format|
-      format.html
-      format.json {render json: @messages.to_json(:only => [:item, :body, :created_at])}
-      format.tsv  {render text: @messages.as_tsv}
-    end
-  end
+		respond_to do |format|
+			format.html
+			format.json {render json: @messages.to_json(:only => [:item, :body, :created_at])}
+			format.tsv  {render text: @messages.as_tsv}
+		end
+	end
 end
