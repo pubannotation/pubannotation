177c1b5559aea47d620550a59d02dd2711c9919a
Jin-Dong Kim <jindong.kim@gmail.com>
2016-01-28 18:53:01 +0900

    HOTFIX sourcedbs fixed to be PubMed, PMC, and FirstAuthor

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -60,8 +60,10 @@ class ProjectsController < ApplicationController
       @docs_count = @project.pmdocs_count + @project.pmcdocs_count
       @docs_count = @project.docs.where(serial: 0).count if @docs_count < 1000
 
-      @sourcedbs = Doc.select(:sourcedb).uniq.pluck(:sourcedb).select{|s| Doc.sourcedb_public?(s) || Doc.sourcedb_mine?(s, current_user)}
-      @sourcedbs_active = @project.docs.select(:sourcedb).uniq.pluck(:sourcedb)
+      # @sourcedbs = Doc.select(:sourcedb).uniq.pluck(:sourcedb).select{|s| Doc.sourcedb_public?(s) || Doc.sourcedb_mine?(s, current_user)}
+      # @sourcedbs_active = @project.docs.select(:sourcedb).uniq.pluck(:sourcedb)
+      @sourcedbs = ['PubMed', 'PMC', 'FirstAuthor']
+      @sourcedbs_active = ['PubMed', 'PMC', 'FirstAuthor']
 
       @annotators = Annotator.all
       @annotator_options = @annotators.map{|a| [a[:abbrev], a[:abbrev]]}
