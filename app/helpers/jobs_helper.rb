6cb0282720d1e964882e2e2626c93f84479ab95e
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2016-11-13 17:39:45 +0900

    ADDED time duration for each job

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -8,4 +8,16 @@ module JobsHelper
 			'Finished'
 		end
 	end
+
+	def time_duration(from, to)
+		t = to - from
+		mm, ss = t.divmod(60)
+		hh, mm = mm.divmod(60)
+		dd, hh = hh.divmod(24)
+		words  = ""
+		words += "#{dd}d " if dd > 0
+		words += "#{hh}h " if hh > 0
+		words += "#{mm}m " if mm > 0
+		words += "#{'%d' % ss}s"
+	end
 end
