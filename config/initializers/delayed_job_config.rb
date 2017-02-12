6119803d0a79cdc13d534339d779407ce914cc26
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2017-02-12 23:25:35 +0900

    ADDED logger setting for delayed jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,3 +2,4 @@ Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_run_time = 14.days
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
