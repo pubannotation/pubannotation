a0b98a21e6a97417240b2103e97e5b46a737ec3c
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2017-04-05 03:02:46 +0900

    REVISED to order the selections

--- app/helpers/annotators_helper.rb
+++ app/helpers/annotators_helper.rb
@@ -1,7 +1,7 @@
 module AnnotatorsHelper
 
 	def annotator_options
-		Annotator.accessibles(current_user).map{|a| [a[:name], a[:name]]}
+		Annotator.accessibles(current_user).order(:name).map{|a| [a[:name], a[:name]]}
 	end
 
   def button_annotator_home
