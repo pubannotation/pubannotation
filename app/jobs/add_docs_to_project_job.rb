0b92f7b2e32e76481ae77ed4f44381e10aaba6dd
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2017-09-01 14:00:57 +0900

    REVISED error messages improved

--- app/jobs/add_docs_to_project_job.rb
+++ app/jobs/add_docs_to_project_job.rb
@@ -5,21 +5,36 @@ class AddDocsToProjectJob < Struct.new(:docspecs, :project)
 		@job.update_attribute(:num_items, docspecs.length)
 		@job.update_attribute(:num_dones, 0)
 
+		@total_num_added = 0
+		@total_num_existed = 0
+
 		docspecs_group_by_sourcedb = docspecs.group_by{|docspec| docspec[:sourcedb]}
+		i = 0
 		docspecs_group_by_sourcedb.each do |sourcedb, docspecs|
 			ids = docspecs.map{|docspec| docspec[:sourceid]}
-			added, messages =
-				begin
-					project.add_docs(sourcedb, ids)
-		    rescue => e
-					@job.messages << Message.create({sourcedb: sourcedb, sourceid: "#{ids.first} - #{ids.last}", body: e.message})
-					[[], []]
-				end
+			added, messages, num_existed = begin
+				project.add_docs(sourcedb, ids)
+			rescue => e
+				@job.messages << Message.create({sourcedb: sourcedb, sourceid: "#{ids.first} - #{ids.last}", body: e.message})
+				[[], [], 0]
+			end
+
+			@total_num_added += added.map{|d| d[:sourceid]}.uniq.length
+			@total_num_existed += num_existed
+
+			@message_docs_existed.update_attribute(:body, "#{@total_num_existed} doc(s) existed. #{@total_num_added} doc(s) added.") if @message_docs_existed
+
+			if @total_num_existed > 0 && !defined?(@message_docs_existed)
+				@message_docs_existed = Message.create({body: "#{@total_num_existed} doc(s) existed. #{@total_num_added} doc(s) added."})
+				@job.messages << @message_docs_existed
+			end
+
 			messages.each do |message|
 				@job.messages << (message.class == Hash ? Message.create(message) : Message.create({body: message}))
 			end
 			num_added_docs = added.map{|d| d[:sourceid]}.uniq.length
-      @job.update_attribute(:num_dones, @job.num_dones + num_added_docs)
+			i += docspecs.length
+			@job.update_attribute(:num_dones, i)
 		end
 		sourcedbs = docspecs_group_by_sourcedb.keys
     unless sourcedbs.empty?
