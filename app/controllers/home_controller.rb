db94fde7fdf7b7a12785364182b408bdc57cfce3
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2020-01-25 14:23:41 +0900

    REVISED the front page to show the news frame only when there is an active news.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,6 @@ class HomeController < ApplicationController
 		@projects_number = Project.accessible(current_user).length
 		@projects_top_annotations_count = Project.accessible(current_user).for_home.top_annotations_count
 		@projects_top_recent = Project.accessible(current_user).for_home.top_recent
-		@news_notifications = NewsNotification.limit(5)
+		@news_notifications = NewsNotification.active.limit(5)
 	end
 end
--- app/models/news_notification.rb
+++ app/models/news_notification.rb
@@ -2,4 +2,5 @@ class NewsNotification < ActiveRecord::Base
   attr_accessible :title, :body, :category, :active
   validates_presence_of :title, :body
   default_scope order: 'updated_at DESC'
+  scope :active, where(active: true)
 end
