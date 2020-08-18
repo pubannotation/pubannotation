56a6d3ce375872e309322232efec8adae153c62a
shar sukhchuluun <sukh@luxiarsukh.local>
2020-08-18 15:39:19 +0900

    issue #19 インデント修正、不要情報削除、readme更新

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-	def login_with_google
+  def login_with_google
 <<HEREDOC
 <div style="display:inline-block; color:#fff">
-    <div style="display:inline-block; margin: 0; padding:5px; background-color:#99f"><i class="fa fa-google" style="width:16px; height:15px"></i></div><div style="display:inline-block; margin:0; padding:5px; background-color:#88f"> Log in with Google</div>
+  <div style="display:inline-block; margin: 0; padding:5px; background-color:#99f"><i class="fa fa-google" style="width:16px; height:15px"></i></div><div style="display:inline-block; margin:0; padding:5px; background-color:#88f"> Log in with Google</div>
 </div>
 HEREDOC
-	end
+  end
 end
