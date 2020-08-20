19f0dc2a50f642f260824563794e820466156b2f
shar sukhchuluun <sukh@luxiarsukh.local>
2020-08-20 15:18:49 +0900

    issue #19 登録済みのユーザはconfirm済とする

--- db/migrate/20200819082927_add_confirmable_to_devise.rb
+++ db/migrate/20200819082927_add_confirmable_to_devise.rb
@@ -5,6 +5,8 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
     add_index :users, :confirmation_token, unique: true
+
+    execute("UPDATE users SET confirmed_at = NOW()")
   end
 
   def down
