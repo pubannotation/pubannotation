3b5d126bd2184f8e58edcef7a0beaa4488421fc8
shsukh <39178089+shsukh@users.noreply.github.com>
2020-08-21 12:01:58 +0900

    Enable Devise confirmation
    * issue #19 usersにconfirm系のカラム追加
    
    * issue #19 gem追加
    
    * issue #19 Deviseのconfirmationを有効に、すでに登録済みのメールアドレスはconfirm済とする
    
    * issue #19 登録済みのユーザはconfirm済とする
    
    * issue #19 登録済みのユーザはconfirmとする
    
    * issue #19 google認証で本登録できる修正
    
    * issue #19 メッセージ変更
    
    Co-authored-by: shar sukhchuluun <sukh@luxiarsukh.local>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20200416132118) do
+ActiveRecord::Schema.define(:version => 20200819082927) do
 
   create_table "annotators", :force => true do |t|
     t.string   "name"
@@ -336,8 +336,13 @@ ActiveRecord::Schema.define(:version => 20200416132118) do
     t.datetime "updated_at",                                :null => false
     t.text     "username",               :default => "",    :null => false
     t.boolean  "root",                   :default => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
