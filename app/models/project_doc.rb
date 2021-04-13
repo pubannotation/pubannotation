342244e36b0fe0d5302d54a13a73762360987839
KishiKyosuke <carp011808pg@icloud.com>
2021-04-13 15:00:32 +0900

    REMOVED attr_accessible of project_doc model

--- app/models/project_doc.rb
+++ app/models/project_doc.rb
@@ -2,8 +2,6 @@ class ProjectDoc < ActiveRecord::Base
 	belongs_to :project
 	belongs_to :doc
 
-	attr_accessible :denotations_num, :relations_num, :modifications_num
-
 	scope :simple_paginate, -> (page, per = 10) {
 		page = page.nil? ? 1 : page.to_i
 		offset = (page - 1) * per
