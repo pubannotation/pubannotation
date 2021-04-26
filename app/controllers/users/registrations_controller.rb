41681e148456031c0fd539b50b930c5da244a907
KishiKyosuke <carp011808pg@icloud.com>
2021-04-26 10:35:46 +0900

    REVISED "for" method in devise controller to "permit" method
    - https://stackoverflow.com/questions/37341967/rails-5-undefined-method-for-for-devise-on-line-devise-parameter-sanitizer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,10 +39,10 @@ before_filter :configure_permitted_parameters
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) do |u|
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
         u.permit(:username, :email, :password, :password_confirmation)
       end
-      devise_parameter_sanitizer.for(:account_update) do |u|
+      devise_parameter_sanitizer.permit(:account_update) do |u|
         u.permit(:username, :email, :password, :password_confirmation, :current_password)
       end
     end
