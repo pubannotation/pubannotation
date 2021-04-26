a5f6c547a0182cf40bcf69cf26336a8fab5c3e7a
KishiKyosuke <carp011808pg@icloud.com>
2021-04-26 10:55:27 +0900

    REVISED `confirm!` to `confirm`

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,7 @@ class ConfirmationsController < Devise::ConfirmationsController
 		resource = resource_class.find_by_confirmation_token(params[:confirmation_token])
 		return super if resource.nil? || resource.confirmed?
 
-		resource.confirm!
+		resource.confirm
 		set_flash_message :notice, :confirmed
 		sign_in_and_redirect(resource)
 	end
