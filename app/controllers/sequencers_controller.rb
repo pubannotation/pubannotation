67e4459cdbe76fb50f5aea6181e603cf849112d1
KishiKyosuke <carp011808pg@icloud.com>
2021-05-10 10:20:07 +0900

    REVISED the process of converting parameters to hash in sequencers_controller.rb
    - Because the timing of serialize type check has changed

--- app/controllers/sequencers_controller.rb
+++ app/controllers/sequencers_controller.rb
@@ -14,7 +14,6 @@ class SequencersController < ApplicationController
 
 	def show
 		@sequencer = Sequencer.find(params[:id])
-		@sequencer.parameters = @sequencer.parameters.map{|p| p.join(' = ')}.join("\n")
 		respond_with(@sequencer)
 	end
 
@@ -30,7 +29,6 @@ class SequencersController < ApplicationController
 	def edit
 		if root_user? || manager?
 			@sequencer = Sequencer.find(params[:id])
-			@sequencer.parameters = @sequencer.parameters.map{|p| p.join(' = ')}.join("\n")
 		else
 			redirect_to root_path, notice: "Unauthorized"
 		end
@@ -38,10 +36,10 @@ class SequencersController < ApplicationController
 
 	def create
 		if root_user? || manager?
-			@sequencer = Sequencer.new(sequencer_params)
-			@sequencer.user = current_user
-			@sequencer.parameters = @sequencer.parameters.delete(' ').split(/[\n\r\t]+/).map{|p| p.split(/[:=]/)}.to_h if @sequencer.parameters.present?
-			@sequencer.save
+			params = sequencer_params
+			params[:parameters] = parameters_to_hash(params[:parameters])
+			params[:user] = current_user
+			@sequencer = Sequencer.create(params)
 			respond_with(@sequencer)
 		else
 			redirect_to root_path, notice: "Unauthorized"
@@ -51,11 +49,11 @@ class SequencersController < ApplicationController
 	def update
 		if root_user? || manager?
 			@sequencer = Sequencer.find(params[:id])
-			update = sequencer_params
-			update['parameters'] = update['parameters'].delete(' ').split(/[\n\r\t]+/).map{|p| p.split(/[:=]/)}.to_h if update['parameters'].present?
+			params = sequencer_params
+			params[:parameters] = parameters_to_hash(params[:parameters])
 
-			@sequencer.update_attributes(update)
-			@sequencer.name = update['name']
+			@sequencer.update_attributes(params)
+			@sequencer.name = params[:name]
 			respond_with(@sequencer)
 		else
 			redirect_to root_path, notice: "Unauthorized"
@@ -80,5 +78,9 @@ class SequencersController < ApplicationController
 	private
 		def sequencer_params
 			params.require(:sequencer).permit(:description, :home, :name, :parameters, :url, :is_public)
+		end
+
+		def parameters_to_hash(parameters)
+			parameters.delete(' ').split(/[\n\r\t]+/).map{|p| p.split(/[:=]/)}.to_h if parameters.present?
 		end
 end
