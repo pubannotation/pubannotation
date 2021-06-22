a49cc128ed3328de7c4fcfed3935cd70e2e13dd7
KishiKyosuke <carp011808pg@icloud.com>
2021-06-22 14:47:46 +0900

    ADDED queue priority settings

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,3 +3,7 @@ Delayed::Worker.max_attempts = 1
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_run_time = 14.days
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.queue_attributes = {
+  general: { priority: 0 },
+  low_priority: { priority: 10 }
+}
