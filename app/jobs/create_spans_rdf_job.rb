dafb345dc42adb911eba0d6acc46e8b07c68ae28
KishiKyosuke <carp011808pg@icloud.com>
2021-07-09 13:49:58 +0900

    ADDED job suspension processing to create_spans_rdf_job.rb

--- app/jobs/create_spans_rdf_job.rb
+++ app/jobs/create_spans_rdf_job.rb
@@ -3,14 +3,14 @@ class CreateSpansRdfJob < ApplicationJob
 
 	def perform(project, in_collection)
 		if @job
-			@job.update_attribute(:num_items, project.docs.count)
-			@job.update_attribute(:num_dones, 0)
+			prepare_progress_record(project.docs.count)
 		end
 
 		project.create_spans_RDF(in_collection) do |i, doc, message|
 			if @job
 				@job.update_attribute(:num_dones, i + 1)
 				@job.messages << Message.create({sourcedb: doc.sourcedb, sourceid: doc.sourceid, body: message}) if message.present?
+				check_suspend_flag
 			end
 		end
 	end
