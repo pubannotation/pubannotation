1446fa575d2d9a44f97f83c7e00d7a269c604996
KishiKyosuke <carp011808pg@icloud.com>
2021-07-09 14:01:47 +0900

    ADDED job suspension processing to update_annotation_numbers_job.rb

--- app/jobs/update_annotation_numbers_job.rb
+++ app/jobs/update_annotation_numbers_job.rb
@@ -11,8 +11,7 @@ class UpdateAnnotationNumbersJob < ApplicationJob
 			{name: "update #modifications of every project_doc", sql:"update project_docs set (modifications_num) = ((select count(*) from modifications inner join denotations on modifications.obj_id=denotations.id and modifications.obj_type='Denotation' where denotations.doc_id = project_docs.id and modifications.project_id=project_docs.project_id) + (select count(*) from modifications inner join relations on modifications.obj_id=relations.id and modifications.obj_type='Relation' inner join denotations on relations.subj_id=denotations.id and relations.subj_type='Denotations' where denotations.doc_id=project_docs.doc_id and modifications.project_id=project_docs.project_id))"}
 		]
 
-		@job.update_attribute(:num_items, queries.count)
-		@job.update_attribute(:num_dones, 0)
+		prepare_progress_record(queries.count)
 
 		queries.each_with_index do |query, i|
 			begin
@@ -20,7 +19,10 @@ class UpdateAnnotationNumbersJob < ApplicationJob
 			rescue => e
 				@job.messages << Message.create({body: "executions of #{query[:name]} failed."})
 			ensure
-				@job.update_attribute(:num_dones, i + 1) if @job
+				if @job
+					@job.update_attribute(:num_dones, i + 1)
+					check_suspend_flag
+				end
 			end
 		end
 	end
