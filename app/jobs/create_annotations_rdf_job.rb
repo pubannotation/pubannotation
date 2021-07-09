084b62824ba26bb4b91a5942f3c521fcf3dc125c
KishiKyosuke <carp011808pg@icloud.com>
2021-07-09 13:48:58 +0900

    ADDED job suspension processing to create_annotations_rdf_job.rb

--- app/jobs/create_annotations_rdf_job.rb
+++ app/jobs/create_annotations_rdf_job.rb
@@ -3,14 +3,14 @@ class CreateAnnotationsRdfJob < ApplicationJob
 
 	def perform(project)
 		if @job
-			@job.update_attribute(:num_items, project.docs.count)
-			@job.update_attribute(:num_dones, 0)
+			prepare_progress_record(project.docs.count)
 		end
 
 		project.create_annotations_RDF do |i, doc, message|
 			if @job
 				@job.update_attribute(:num_dones, i + 1)
 				@job.messages << Message.create({sourcedb: doc.sourcedb, sourceid: doc.sourceid, body: message}) if message
+				check_suspend_flag
 			end
 		end
 	end
