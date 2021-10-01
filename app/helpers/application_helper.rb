173778039c032f54e5b5fcabce7eb8ef7cd7660d
Jin-Dong Kim <jindong.kim@gmail.com>
2021-10-01 09:44:09 +0900

    FIXED the pagination to include sort_key and sort_direction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,10 +23,10 @@ module ApplicationHelper
 	def simple_paginate
 		current_page = params[:page].nil? ? 1 : params[:page].to_i
 		nav = ''
-		nav += link_to(content_tag(:i, '', class: "fa fa-angle-double-left", "aria-hidden" => "true"), params.permit(:controller, :action).except(:page), title: "First", class: 'page') if current_page > 2
-		nav += link_to(content_tag(:i, '', class: "fa fa-angle-left", "aria-hidden" => "true"), params.permit(:controller, :action).merge(page: current_page - 1), title: "Previous", class: 'page') if current_page > 1
+		nav += link_to(content_tag(:i, '', class: "fa fa-angle-double-left", "aria-hidden" => "true"), params.permit(:controller, :action, :sort_key, :sort_direction).except(:page), title: "First", class: 'page') if current_page > 2
+		nav += link_to(content_tag(:i, '', class: "fa fa-angle-left", "aria-hidden" => "true"), params.permit(:controller, :action, :sort_key, :sort_direction).merge(page: current_page - 1), title: "Previous", class: 'page') if current_page > 1
 		nav += content_tag(:span, "Page #{current_page}", class: 'page')
-		nav += link_to(content_tag(:i, '', class: "fa fa-angle-right", "aria-hidden" => "true"), params.permit(:controller, :action).merge(page: current_page + 1), title: "Next", class: 'page') unless params[:last_page]
+		nav += link_to(content_tag(:i, '', class: "fa fa-angle-right", "aria-hidden" => "true"), params.permit(:controller, :action, :sort_key, :sort_direction).merge(page: current_page + 1), title: "Next", class: 'page') unless params[:last_page]
 		content_tag(:nav, nav.html_safe, class: 'pagination')
 	end
 
