2575321f44ab44fb914d15e722fb61729f38c93b
Jin-Dong Kim <jindong.kim@gmail.com>
2021-10-21 23:09:58 +0900

    FIXED to add access control to the 'edit' and 'delete' buttons for queries

--- app/models/query.rb
+++ app/models/query.rb
@@ -3,4 +3,9 @@ class Query < ActiveRecord::Base
 
 	# type
 	# 0: project-independent, 1: project-dependent, 2: project-specific
+
+	def editable?(current_user)
+		current_user.present? && (current_user.root? || (organization.present? && current_user == organization.user))
+	end
+
 end
