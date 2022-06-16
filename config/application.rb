a0b5c6c779ab871dade966660e3ba2cff6fd050a
ledsun <shigeru.nakajima@gmail.com>
2022-06-16 18:18:07 +0900

    Disable the active_storage

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'active_record/railtie'
-require 'active_storage/engine'
+#require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'action_mailer/railtie'
