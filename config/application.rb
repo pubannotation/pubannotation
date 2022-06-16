87de1e7a82f855c575dffd7e016a12d5eeabd253
ledsun <shigeru.nakajima@gmail.com>
2022-06-16 14:24:27 +0900

    Delete the encoding setting that is UTF-8 by default

--- config/application.rb
+++ config/application.rb
@@ -49,9 +49,6 @@ module Pubann
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
