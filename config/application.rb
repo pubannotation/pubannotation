f2633abf9fa1fa9e02411e12d51a0839e0c42159
ledsun <shigeru.nakajima@gmail.com>
2022-06-16 14:17:28 +0900

    `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb

--- config/application.rb
+++ config/application.rb
@@ -47,8 +47,5 @@ module Pubann
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
