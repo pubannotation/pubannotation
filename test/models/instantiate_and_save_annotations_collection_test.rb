f913154398d6dd2634774a3b986f6c1bd3f4e4d2
ledsun <shigeru.nakajima@gmail.com>
2022-07-11 19:23:25 +0900

    Add assertions into the InstantiateAndSaveAnnotationsCollectionTest

--- test/models/instantiate_and_save_annotations_collection_test.rb
+++ test/models/instantiate_and_save_annotations_collection_test.rb
@@ -6,6 +6,16 @@ class InstantiateAndSaveAnnotationsCollectionTest < ActiveSupport::TestCase
   end
 
   test "instantiate_and_save_annotations_collection" do
-    InstantiateAndSaveAnnotationsCollection.call @project, []
+    current = Time.zone.local(2022, 7, 11, 10, 04, 44)
+    travel_to current do
+      InstantiateAndSaveAnnotationsCollection.call @project, []
+    end
+
+    @project.reload
+    assert_equal 0, @project.denotations_num
+    assert_equal 0, @project.relations_num
+    assert_equal 0, @project.modifications_num
+    assert_equal current, @project.updated_at
+    assert_equal current, @project.annotations_updated_at
   end
 end
