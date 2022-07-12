93aa168dc993fd990a250ba274a0939b7c7f88db
ledsun <shigeru.nakajima@gmail.com>
2022-07-12 09:48:41 +0900

    Add test to import one denotation

--- test/models/instantiate_and_save_annotations_collection_test.rb
+++ test/models/instantiate_and_save_annotations_collection_test.rb
@@ -5,7 +5,7 @@ class InstantiateAndSaveAnnotationsCollectionTest < ActiveSupport::TestCase
     @project = projects(:one)
   end
 
-  test "instantiate_and_save_annotations_collection" do
+  test "update to empty annotations" do
     current = Time.zone.local(2022, 7, 11, 10, 04, 44)
     travel_to current do
       InstantiateAndSaveAnnotationsCollection.call @project, []
@@ -13,6 +13,25 @@ class InstantiateAndSaveAnnotationsCollectionTest < ActiveSupport::TestCase
 
     @project.reload
     assert_equal 0, @project.denotations_num
+    assert_equal 0, @project.relations_num
+    assert_equal 0, @project.modifications_num
+    assert_equal current, @project.updated_at
+    assert_equal current, @project.annotations_updated_at
+  end
+
+  test "update to non-empty annotations" do
+    current = Time.zone.local(2022, 7, 11, 10, 04, 44)
+    travel_to current do
+      annotations_collection = [{sourcedb: "test_db", sourceid: "1234",
+                                 denotations: [
+                                   {id: "1", span: { begin: 1, end: 2 }, obj: "A"}
+                                 ]
+                                }]
+      InstantiateAndSaveAnnotationsCollection.call @project, annotations_collection
+    end
+
+    @project.reload
+    assert_equal 1, @project.denotations_num
     assert_equal 0, @project.relations_num
     assert_equal 0, @project.modifications_num
     assert_equal current, @project.updated_at
