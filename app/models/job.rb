2452271f8f6b23d7747c8ae6ae067ccc39da83d2
ledsun <shigeru.nakajima@gmail.com>
2022-07-27 15:34:00 +0900

    Format codes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,64 +1,63 @@
 class Job < ActiveRecord::Base
-	belongs_to :organization, polymorphic: true
-	has_many :messages, :dependent => :destroy
+  belongs_to :organization, polymorphic: true
+  has_many :messages, :dependent => :destroy
 
-	scope :waiting, -> {where('begun_at IS NULL')}
-	scope :running, -> {where('begun_at IS NOT NULL AND ended_at IS NULL')}
-	scope :unfinished, -> {where('ended_at IS NULL')}
-	scope :finished, -> {where('ended_at IS NOT NULL')}
+  scope :waiting, -> { where('begun_at IS NULL') }
+  scope :running, -> { where('begun_at IS NOT NULL AND ended_at IS NULL') }
+  scope :unfinished, -> { where('ended_at IS NULL') }
+  scope :finished, -> { where('ended_at IS NOT NULL') }
 
+  def waiting?
+    begun_at.nil?
+  end
 
-	def waiting?
-		begun_at.nil?
-	end
+  def running?
+    !begun_at.nil? && ended_at.nil?
+  end
 
-	def running?
-		!begun_at.nil? && ended_at.nil?
-	end
+  def finished?
+    !ended_at.nil?
+  end
 
-	def finished?
-		!ended_at.nil?
-	end
+  def finished_live?
+    !ActiveRecord::Base.connection.select_value("select ended_at from jobs where id = #{id}").nil?
+  end
 
-	def finished_live?
-		!ActiveRecord::Base.connection.select_value("select ended_at from jobs where id = #{id}").nil?
-	end
+  def unfinished?
+    ended_at.nil?
+  end
 
-	def unfinished?
-		ended_at.nil?
-	end
+  def suspended?
+    running? && suspend_flag == true
+  end
 
-	def suspended?
-		running? && suspend_flag == true
-	end
+  def state
+    if begun_at.nil?
+      'Waiting'
+    elsif ended_at.nil?
+      'Running'
+    else
+      'Finished'
+    end
+  end
+  
+  def destroy_if_not_running
+    case state
+    when 'Waiting'
+      ApplicationJob.cancel_adapter_class.new.cancel(active_job_id, queue_name)
+      Message.where(job_id: id).delete_all
+      destroy
+    when 'Finished'
+      Message.where(job_id: id).delete_all
+      destroy
+    when 'Running'
+      # do nothing
+    end
+  end
 
-	def state
-		if begun_at.nil?
-			'Waiting'
-		elsif ended_at.nil?
-			'Running'
-		else
-			'Finished'
-		end
-	end
-
-	def destroy_if_not_running
-		case state
-		when 'Waiting'
-			ApplicationJob.cancel_adapter_class.new.cancel(active_job_id, queue_name)
-			Message.where(job_id: id).delete_all
-			destroy
-		when 'Finished'
-			Message.where(job_id: id).delete_all
-			destroy
-		when 'Running'
-			# do nothing
-		end
-	end
-
-	def stop_if_running
-		if running?
-			update(suspend_flag: true)
-		end
-	end
+  def stop_if_running
+    if running?
+      update(suspend_flag: true)
+    end
+  end
 end
