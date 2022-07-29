72b39b36a88947cfe46635038f5324d24a2bcaf8
ledsun <shigeru.nakajima@gmail.com>
2022-07-29 16:02:34 +0900

    Create Message at the end of the job

--- app/jobs/add_docs_to_project_job.rb
+++ app/jobs/add_docs_to_project_job.rb
@@ -31,15 +31,6 @@ class AddDocsToProjectJob < ApplicationJob
       @total_num_sequenced += num_sequenced
       @total_num_existed += num_existed
 
-      @message_docs_existed.update_attribute(:body, "#{@total_num_existed} doc(s) existed. #{@total_num_added} doc(s) added.") if @message_docs_existed
-
-      if @total_num_existed > 0 && !defined?(@message_docs_existed)
-        @message_docs_existed = Message.create({ body: "#{@total_num_existed} doc(s) existed. #{@total_num_added} doc(s) added." })
-        if @job
-          @job.messages << @message_docs_existed
-        end
-      end
-
       unless messages.empty?
         if @job
           messages.each do |message|
@@ -67,6 +58,11 @@ class AddDocsToProjectJob < ApplicationJob
       ActionController::Base.new.expire_fragment("sourcedb_counts_#{project.name}")
       ActionController::Base.new.expire_fragment("count_docs_#{project.name}")
       sourcedbs.uniq.each { |sdb| ActionController::Base.new.expire_fragment("count_#{sdb}_#{project.name}") }
+    end
+
+  ensure
+    if @total_num_existed > 0
+      @job&.add_message body: "#{@total_num_existed} doc(s) existed. #{@total_num_added} doc(s) added."
     end
   end
 
