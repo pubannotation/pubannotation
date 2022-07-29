66f783f684eac46226d27cd5a047c1a48c5220f3
ledsun <shigeru.nakajima@gmail.com>
2022-07-29 16:28:09 +0900

    Inline the create_job_records that is called from one place

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -22,7 +22,7 @@ class ApplicationJob < ActiveJob::Base
   def before_enqueue
     # When creating a new job,
     # be sure to pass the organization to which the Job record belongs to the first argument of the perform method.
-    create_job_record(self.arguments.first.jobs, self.job_name)
+    self.arguments.first.jobs.create({ name: self.job_name, active_job_id: self.job_id, queue_name: self.queue_name })
   end
 
   def before_perform(active_job)
@@ -36,10 +36,6 @@ class ApplicationJob < ActiveJob::Base
 
   def resource_name
     self.arguments.first.name
-  end
-
-  def create_job_record(organization_jobs, job_name)
-    organization_jobs.create({ name: job_name, active_job_id: self.job_id, queue_name: self.queue_name })
   end
 
   def check_suspend_flag
