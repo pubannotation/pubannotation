f291ef3d23deeec7afc366785faaa3e3c4d8a922
ledsun <shigeru.nakajima@gmail.com>
2022-08-03 09:51:56 +0900

    Extract the AnnotationCollection class

--- app/jobs/store_annotations_collection_upload_job.rb
+++ app/jobs/store_annotations_collection_upload_job.rb
@@ -1,48 +1,4 @@
 class StoreAnnotationsCollectionUploadJob < ApplicationJob
-  class AnnotationCollection
-    attr_reader :annotations, :sourcedb, :sourceid
-    def initialize(json_file_path)
-      @annotations = load_from json_file_path
-      set_sourcedb_and_sourceid @annotations
-      validate_and_normalize! @annotations
-    end
-
-    def has_denotation?
-      number_of_denotations > 0
-    end
-    def number_of_denotations
-      @annotations.map { _1[:denotations].present? ? _1[:denotations].size : 0 }
-                            .sum
-    end
-
-    private
-
-    def load_from(json_file_path)
-      json = File.read(json_file_path)
-      o = JSON.parse(json, symbolize_names: true)
-
-      # To return the annotation in an array
-      o.is_a?(Array) ? o : [o]
-    rescue JSON::ParserError
-      raise "[#{File.basename(json_file_path)}] JSON parse error. Not a valid JSON object."
-    end
-
-    def set_sourcedb_and_sourceid(annotations)
-      annotation = annotations.first
-      raise ArgumentError, "sourcedb and/or sourceid not specified." unless annotation[:sourcedb].present? && annotation[:sourceid].present?
-      @sourcedb = annotation[:sourcedb]
-      @sourceid = annotation[:sourceid]
-    end
-
-    def validate_and_normalize!(annotations)
-      annotations.each do |annotation|
-        raise ArgumentError, "One json file has to include annotations to the same document." if (annotation[:sourcedb] != @sourcedb) || (annotation[:sourceid] != @sourceid)
-
-        Annotation.normalize!(annotation)
-      end
-    end
-  end
-
   queue_as :low_priority
 
   MAX_SIZE_TRANSACTION = 5000
