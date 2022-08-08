214700a7046eb8a4d90940131247d14f9113f9b2
ledsun <shigeru.nakajima@gmail.com>
2022-08-08 14:36:42 +0900

    When debugging, record the location where the error occurred

--- app/jobs/store_annotations_collection_upload_job.rb
+++ app/jobs/store_annotations_collection_upload_job.rb
@@ -58,9 +58,10 @@ class StoreAnnotationsCollectionUploadJob < ApplicationJob
       raise
     rescue StandardError => e
       if @job
+        body = Rails.env.development? ? "#{e.backtrace_locations[0..2] } #{e.message[0..250]}" : e.message[0..250]
         @job.add_message sourcedb: annotation_collection.sourcedb,
                          sourceid: annotation_collection.sourceid,
-                         body: e.message[0..250]
+                         body: body
       else
         raise ArgumentError, "[#{annotation_collection.sourcedb}:#{annotation_collection.sourceid}] #{e.message}"
       end
