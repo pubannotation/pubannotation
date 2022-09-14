6165b265bd8dcec008c4e123274bd67362baac7c
ledsun <shigeru.nakajima@gmail.com>
2022-09-14 16:41:10 +0900

    Extract the AnntationTransaction class

--- app/jobs/store_annotations_collection_upload_job.rb
+++ app/jobs/store_annotations_collection_upload_job.rb
@@ -1,28 +1,5 @@
-class AnnotationTransaction
-  MAX_SIZE_TRANSACTION = 5000
-
-  attr_reader :sourcedb_sourceids_index, :annotation_transaction
-
-  def initialize
-    @annotation_transaction = []
-    @transaction_size = 0
-    @sourcedb_sourceids_index = Hash.new(Set.new)
-  end
-
-  def <<(annotation_collection)
-    @annotation_transaction << annotation_collection.annotations
-    @transaction_size += annotation_collection.number_of_denotations
-    @sourcedb_sourceids_index[annotation_collection.sourcedb] << annotation_collection.sourceid
-  end
-
-  def enough?
-    @transaction_size > MAX_SIZE_TRANSACTION
-  end
-end
-
 class StoreAnnotationsCollectionUploadJob < ApplicationJob
   queue_as :low_priority
-
 
   def perform(project, filepath, options)
     # read the filenames of json files into the array filenames
