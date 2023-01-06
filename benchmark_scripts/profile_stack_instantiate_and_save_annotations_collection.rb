b774fee94205f562516063312be96e28bdd6608e
ledsun <shigeru.nakajima@gmail.com>
2023-01-06 17:19:25 +0900

    Fix a file path in the benchmark script

--- benchmark_scripts/profile_stack_instantiate_and_save_annotations_collection.rb
+++ benchmark_scripts/profile_stack_instantiate_and_save_annotations_collection.rb
@@ -1,4 +1,4 @@
-file = './PMC-7646410.json'
+file = './benchmark_scripts/PMC-7646410.json'
 annotations_collection = JSON.parse File.read(file)
 annotations_collection.each { _1.deep_symbolize_keys! }
 project = Project.find_by(id: 1)
