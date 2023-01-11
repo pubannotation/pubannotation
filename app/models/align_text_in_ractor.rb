0a28347bf199eecaef531bff48a3da2f6c562837
ledsun <shigeru.nakajima@gmail.com>
2023-01-11 17:19:30 +0900

    Use Data classes

--- app/models/align_text_in_ractor.rb
+++ app/models/align_text_in_ractor.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
 class AlignTextInRactor
+  class Results
+    attr_reader :index, :processed_annotations
+    def initialize(index, processed_annotations)
+      @index = index
+      @processed_annotations = processed_annotations
+    end
+  end
+
+  class ProcessedAnnotation
+    attr_reader :denotations, :blocks, :lost_annotations, :block_alignments, :error_message
+    def initialize(denotations, blocks, lost_annotations, block_alignments, error_message = nil)
+      @denotations = denotations
+      @blocks = blocks
+      @lost_annotations = lost_annotations
+      @block_alignments = block_alignments
+      @error_message = error_message
+    end
+  end
+
   attr_reader :annotations_for_doc_collection, :messages
   def initialize(annotations_for_doc_collection, options)
     @annotations_for_doc_collection = annotations_for_doc_collection
@@ -23,23 +42,16 @@ class AlignTextInRactor
             begin
               aligner.align(datum[:text], datum[:denotations] + datum[:blocks])
 
-              {
-                denotations: aligner.transform_hdenotations(datum[:denotations]),
-                blocks: aligner.transform_hdenotations(datum[:blocks]),
-                lost_annotations: aligner.lost_annotations,
-                block_alignment: aligner.lost_annotations.present? ? aligner.block_alignment : nil
-              }
+              ProcessedAnnotation.new aligner.transform_hdenotations(datum[:denotations]),
+                                      aligner.transform_hdenotations(datum[:blocks]),
+                                      aligner.lost_annotations,
+                                      aligner.lost_annotations.present? ? aligner.block_alignment : nil
             rescue => e
-              break {
-                error: e.message
-              }
+              break ProcessedAnnotation.new(nil, nil, nil, nil, e.message)
             end
           end
 
-          Ractor.yield(Ractor.make_shareable({
-                                               index: msg.index,
-                                               results: results
-                                             }))
+          Ractor.yield(Ractor.make_shareable(Results.new(msg.index, results)))
         end
       end
     end
@@ -66,36 +78,31 @@ class AlignTextInRactor
     end.each do
       _r, results = Ractor.select(*workers)
 
-      annotations, doc = annotations_for_doc_collection[results[:index]]
+      annotations, doc = annotations_for_doc_collection[results.index]
       ref_text = doc&.original_body || doc.body
       targets = annotations.filter {|a| a[:denotations].present? || a[:blocks].present? }
 
-      @messages << results[:results].map.with_index do |result, i|
+      results.processed_annotations.each.with_index do |processed_annotation, i|
         annotation = targets[i]
+        raise "[#{annotation[:sourcedb]}:#{annotation[:sourceid]}] #{processed_annotation.error_message}" if processed_annotation.error_message
 
-        if result[:error]
-          raise "[#{annotation[:sourcedb]}:#{annotation[:sourceid]}] #{result[:error].message}"
-        else
-          annotation[:denotations] = result[:denotations]
-          annotation[:blocks] = result[:blocks]
-          annotation[:text] = ref_text
-          annotation.delete_if{|k,v| !v.present?}
+        annotation[:denotations] = processed_annotation.denotations
+        annotation[:blocks] = processed_annotation.blocks
+        annotation[:text] = ref_text
+        annotation.delete_if{|k,v| !v.present?}
 
-          if result[:lost_annotations].present?
-            {
-              sourcedb: annotation[:sourcedb],
-              sourceid: annotation[:sourceid],
-              body:"Alignment failed. Invalid denotations found after transformation",
-              data:{
-                block_alignment: result[:block_alignment],
-                lost_annotations: result[:lost_annotations]
-              }
+        if processed_annotation.lost_annotations.present?
+          @messages << {
+            sourcedb: annotation[:sourcedb],
+            sourceid: annotation[:sourceid],
+            body:"Alignment failed. Invalid denotations found after transformation",
+            data:{
+              block_alignment: processed_annotation.block_alignment,
+              lost_annotations: processed_annotation.lost_annotations
             }
-          else
-            nil
-          end
+          }
         end
-      end.compact
+      end
     end
   end
 end
