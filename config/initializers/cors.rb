047e3b281645d0e6db095fa521a73fd4d5171156
jdkim <jindong.kim@gmail.com>
2023-05-24 22:44:57 +0900

    REVISED to allow any CORS origin except "null"

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://textae.pubannotation.org', 'http://localhost:3000'
-
-    # credentials (boolean, default: false): Sets the Access-Control-Allow-Credentials response header.
-    resource '*', methods: :any, headers: :any, credentials: true
-  end
+	allow do
+		origins do |source, env|
+			source unless source == 'null'
+		end
+		resource '*', methods: :any, headers: :any, credentials: true
+	end
 end
