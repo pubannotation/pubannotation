a7927fdc4618e808ca845c06b1bdb5028b5f5c89
jdkim <jindong.kim@gmail.com>
2023-08-09 02:17:55 +0900

    REVISED for performance

--- app/jobs/delete_all_docs_from_project_job.rb
+++ app/jobs/delete_all_docs_from_project_job.rb
@@ -6,9 +6,43 @@ class DeleteAllDocsFromProjectJob < ApplicationJob
 			prepare_progress_record(1)
 		end
 
-		project.delete_docs
+		ActiveRecord::Base.transaction do
+			# 1. delete all the annotations
+			project.delete_annotations if project.denotations_num > 0
 
-		UpdateElasticsearchIndexJob.perform_later(project)
+			if project.docs.exists?
+				# 2. mark the docs to be deleted, and decrement projects_num
+				ActiveRecord::Base.connection.exec_query(
+					"
+						UPDATE docs
+						SET projects_num = projects_num - 1, flag = true
+						WHERE docs.id
+						IN (
+							SELECT project_docs.doc_id
+							FROM project_docs
+							WHERE project_docs.project_id = #{project.id}
+						)
+					"
+				)
+
+				# 3. delete the association
+				ActiveRecord::Base.connection.exec_query("DELETE FROM project_docs WHERE project_id = #{project.id}")
+
+				# 4. destroy orphan custom documents
+				wclause = "sourcedb LIKE '%#{Doc::UserSourcedbSeparator}#{project.user.username}' AND projects_num = 0"
+				Doc.where(wclause).ids.each do |doc_id|
+					Indexer.perform_later(nil, :delete, doc_id)
+				end
+				ActiveRecord::Base.connection.exec_query("DELETE FROM docs WHERE " + wclause)
+
+				# 5. update ES index
+				Doc.where(flag: true).ids.each{|doc_id| Indexer.perform_later(nil, :update, doc_id)}
+				# Doc.__elasticsearch__.import query: -> { where(flag: true) }
+			end
+		end
+
+		# 6. clear flags
+		ActiveRecord::Base.connection.exec_query('UPDATE docs SET flag = false WHERE flag = true')
 
 		@job.update_attribute(:num_dones, 1) if @job
 		ActionController::Base.new.expire_fragment("sourcedb_counts_#{project.name}")
