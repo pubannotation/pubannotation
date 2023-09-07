094ef7a5794540424814ccd413421e297484d5ef
ledsun <shigeru.nakajima@gmail.com>
2023-09-07 14:04:56 +0900

    Add test case

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'test.pubannotation.org' }
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
@@ -55,4 +55,6 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  Rails.application.routes.default_url_options[:host]= 'test.pubannotation.org'
 end
--- spec/requests/annotations_spec.rb
+++ spec/requests/annotations_spec.rb
@@ -8,9 +8,31 @@ RSpec.describe "Annotations", type: :request do
         expect(response).to have_http_status(404)
       end
 
-      it "returns JSON message" do
+      it "returns JSON" do
         get doc_sourcedb_sourceid_path sourcedb: 'abc', sourceid: '123', begin: 1, end: 2, format: :json
         expect(response.body).to eq({ message: 'File not found.' }.to_json)
+      end
+    end
+
+    context 'when document is found' do
+      before do
+        create(:doc)
+      end
+
+      it 'returns 200 response' do
+        get doc_sourcedb_sourceid_path sourcedb: 'PubMed', sourceid: '12345678', begin: 1, end: 2, format: :json
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns JSON' do
+        get doc_sourcedb_sourceid_path sourcedb: 'PubMed', sourceid: '12345678', begin: 1, end: 2, format: :json
+        expect(response.body).to eq({
+                                      target: 'http://test.pubannotation.org/docs/sourcedb/PubMed/sourceid/12345678' ,
+                                      sourcedb: 'PubMed',
+                                      sourceid: '12345678',
+                                      text: "h",
+                                      tracks: []
+                                    }.to_json)
       end
     end
   end
