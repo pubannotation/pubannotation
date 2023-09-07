85ee87b7fe55985a0349e9b6307d06483a7fbcd8
ledsun <shigeru.nakajima@gmail.com>
2023-09-07 14:37:36 +0900

    Add test for the Doc#get_project_annotations

--- spec/models/doc_spec.rb
+++ spec/models/doc_spec.rb
@@ -28,4 +28,17 @@ RSpec.describe Doc, type: :model do
       expect(doc.hannotations[:tracks]).to eq([])
     end
   end
+
+  describe 'get_project_annotations' do
+    let(:doc) { create(:doc) }
+    let(:project) { create(:project) }
+
+    it 'returns an array' do
+      expect(doc.get_project_annotations(project)).to be_a(Hash)
+    end
+
+    it 'returns an array with project' do
+      expect(doc.get_project_annotations(project)[:project]).to eq('TestProject')
+    end
+  end
 end
