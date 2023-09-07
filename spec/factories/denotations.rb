4e9d850ac5d52b1ba702378c40a5782ede53b24f
ledsun <shigeru.nakajima@gmail.com>
2023-09-07 15:49:36 +0900

    Add test when there is denotation

---
+++ spec/factories/denotations.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :denotation do
+    project
+    doc
+    hid { 'T1' }
+    add_attribute(:begin) { 0 }
+    add_attribute(:end) { 4 }
+    obj { 'Test' }
+  end
+end
--- spec/models/doc_spec.rb
+++ spec/models/doc_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Doc, type: :model do
   end
 
   describe 'get_denotations' do
-    subject { doc.get_denotations(project) }
+    subject { doc.get_denotations(project.id) }
     let(:doc) { create(:doc) }
     let(:project) { create(:project) }
 
@@ -53,6 +53,18 @@ RSpec.describe Doc, type: :model do
 
     context 'when there are no denotations' do
       it { is_expected.to be_empty }
+    end
+
+    context 'when there are denotations' do
+      before do
+        create(:denotation, doc: doc, project: project)
+      end
+
+      it { is_expected.not_to be_empty }
+
+      it 'returns an array of denotations' do
+        expect(subject.first).to be_a(Denotation)
+      end
     end
   end
 end
