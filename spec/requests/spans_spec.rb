0007c19f1fd6d6bd9e50a1db78507cd47187a085
ledsun <shigeru.nakajima@gmail.com>
2023-09-13 17:55:56 +0900

    Add test for SpansController#project_doc_spans_index

--- spec/requests/spans_spec.rb
+++ spec/requests/spans_spec.rb
@@ -54,4 +54,41 @@ RSpec.describe "Spans", type: :request do
       end
     end
   end
+
+  describe "GET /projects/:project_id/docs/sourcedb/:sourcedb/sourceid/:sourceid/spans" do
+    context 'when project is not found' do
+      it "returns 422 response" do
+        get spans_index_project_sourcedb_sourceid_docs_path project_id: 0, sourcedb: 'abc', sourceid: '123', format: :json
+        expect(response).to have_http_status(422)
+      end
+    end
+
+    context 'when project is found' do
+      let(:project) { create(:project, accessibility: 1) }
+
+      context 'when document is not found' do
+        it "returns 422 response" do
+          get spans_index_project_sourcedb_sourceid_docs_path project_id: project.name, sourcedb: 'abc', sourceid: '123', format: :json
+          expect(response).to have_http_status(422)
+        end
+      end
+
+      context 'when document is found' do
+        let(:doc) { create(:doc) }
+        let!(:project_doc) { create(:project_doc, project: project, doc: doc) }
+
+        it "returns 200 response" do
+          get spans_index_project_sourcedb_sourceid_docs_path project_id: project.name, sourcedb: 'PubMed', sourceid: doc.sourceid, format: :json
+
+          expect(response).to have_http_status(200)
+        end
+
+        it "returns text of doc" do
+          get spans_index_project_sourcedb_sourceid_docs_path project_id: project.name, sourcedb: 'PubMed', sourceid: doc.sourceid, format: :json
+
+          expect(JSON.parse(response.body)['text']).to eq(doc.body)
+        end
+      end
+    end
+  end
 end
