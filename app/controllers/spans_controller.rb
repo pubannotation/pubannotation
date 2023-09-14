3c7f241116c9429b1b94ba9d321e8603d8e446df
ledsun <shigeru.nakajima@gmail.com>
2023-09-14 14:05:58 +0900

    Format style

--- app/controllers/spans_controller.rb
+++ app/controllers/spans_controller.rb
@@ -62,24 +62,24 @@ class SpansController < ApplicationController
   end
   
   def sql
-    begin
-      @search_path = spans_sql_path
-      if params[:project_id].present?
-        # when search from inner project
-        project = Project.find_by_name(params[:project_id])
-        if project.present?
-          @search_path = project_spans_sql_path
-        else
-          redirect_to @search_path
-        end
+    @search_path = spans_sql_path
+
+    if params[:project_id].present?
+      # when search from inner project
+      project = Project.find_by_name(params[:project_id])
+      if project.present?
+        @search_path = project_spans_sql_path
+      else
+        redirect_to @search_path
       end
-      @denotations = Denotation.sql_find(params, current_user, project ||= nil)
-      if @denotations.present?
-        @denotations = @denotations.page(params[:page]).per(50)
-      end
-    rescue => error
-      flash[:notice] = "#{t('controllers.shared.sql.invalid')} #{error}"
     end
+
+    @denotations = Denotation.sql_find(params, current_user, project ||= nil)
+    if @denotations.present?
+      @denotations = @denotations.page(params[:page]).per(50)
+    end
+  rescue => error
+    flash[:notice] = "#{t('controllers.shared.sql.invalid')} #{error}"
   end
 
   def get_url
