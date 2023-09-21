b52332b9cc7c1b4c6943a9a5a10eb74d20ab8bf5
ledsun <shigeru.nakajima@gmail.com>
2023-09-21 15:03:52 +0900

    Add test to check data for Doc#get_project_denotation

--- spec/models/doc/get_project_annotations_spec.rb
+++ spec/models/doc/get_project_annotations_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Doc, type: :model do
    describe 'get_project_annotations' do
-    let(:doc) { create(:doc) }
-    let(:project) { create(:project) }
+     let(:doc) { create(:doc) }
+     let(:project) { create(:project) }
 
-    it 'returns an array' do
+    before do
+      create(:project_doc, doc: doc, project: project)
+      create(:denotation, doc: doc, project: project)
+      create(:block, doc: doc, project: project)
+    end
+
+    it 'returns an hash' do
       expect(doc.get_project_annotations(project)).to be_a(Hash)
     end
 
-    it 'returns an array with project' do
+    it 'returns an hash with project' do
       expect(doc.get_project_annotations(project)[:project]).to eq('TestProject')
     end
+
+    it 'returns an hash with denotations' do
+      expect(doc.get_project_annotations(project)[:denotations]).to include(id: "T1", obj: 'subject', span: {begin: 0, end: 4})
+    end
+
+     it 'returns an hash with blocks' do
+        expect(doc.get_project_annotations(project)[:blocks]).to include(id: "B1", obj: '1st line', span: {begin: 0, end: 14})
+     end
   end
 end
