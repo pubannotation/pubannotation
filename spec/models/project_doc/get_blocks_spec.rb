1fe31018c376aefb1e00b1dd3d7029646c52e3f4
ledsun <shigeru.nakajima@gmail.com>
2023-09-25 18:10:41 +0900

    Refactor test code

--- spec/models/project_doc/get_blocks_spec.rb
+++ spec/models/project_doc/get_blocks_spec.rb
@@ -11,65 +11,53 @@ RSpec.describe ProjectDoc, type: :model do
     let(:context_size) { nil }
     let(:sort) { false }
 
-    it 'returns an array' do
-      is_expected.to be_a(ActiveRecord::AssociationRelation)
-    end
+    it { is_expected.to be_a(ActiveRecord::AssociationRelation) }
 
     context 'when there are no blocks' do
       it { is_expected.to be_empty }
     end
 
     context 'when there are blocks' do
-      before do
-        create(:block, doc: doc, project: project)
-        create(:third_block, doc: doc, project: project)
-        create(:second_block, doc: doc, project: project)
-      end
+      let!(:block) { create(:block, doc: doc, project: project) }
+      let!(:third_block) { create(:third_block, doc: doc, project: project) }
+      let!(:second_block) { create(:second_block, doc: doc, project: project) }
 
       it { is_expected.not_to be_empty }
 
-      it 'returns an array of blocks' do
-        expect(subject.first).to be_a(Block)
-      end
-
-      it 'return an array of blocks sorted by creation order' do
+      it 'returns blocks' do
+        expect(subject).to all(be_a(Block))
         expect(subject.second.hid).to eq('B3')
         expect(subject.third.hid).to eq('B2')
       end
 
-      context 'when span is specified' do
+      context 'with specified span' do
         let(:span) { {begin: 10, end: 40} }
 
-        it 'returns an array of blocks between the specified span' do
+        it 'returns blocks within the span and adjusts by span and context_size' do
+          adjusted_begin = 6 + (context_size || 0)
+          adjusted_end = 27 + (context_size || 0)
+
           expect(subject.first.hid).to eq('B2')
+          expect(subject.first.begin).to eq(adjusted_begin)
+          expect(subject.first.end).to eq(adjusted_end)
         end
 
-        it 'returns an array of blocks offset by the specified span' do
-          expect(subject.first.begin).to eq(6)
-          expect(subject.first.end).to eq(27)
-        end
-
-        context 'when context_size is specified' do
+        context 'with specified context_size' do
           let(:context_size) { 6 }
 
-          it 'returns an array of blocks offset by the specified span and context_size' do
-            expect(subject.first.begin).to eq(6 + context_size)
-            expect(subject.first.end).to eq(27 + context_size)
-          end
-
-          context 'when context_size equals to begin of the span' do
+          context 'equal to begin of the span' do
             let(:context_size) { 10 }
 
-            it 'returns an array of blocks without offset' do
+            it 'returns blocks without offset' do
               expect(subject.first.begin).to eq(16)
               expect(subject.first.end).to eq(37)
             end
           end
 
-          context 'when context_size is bigger than begin of the span' do
+          context 'bigger than begin of the span' do
             let(:context_size) { 11 }
 
-            it 'returns an array of blocks without offset' do
+            it 'returns blocks without offset' do
               expect(subject.first.begin).to eq(16)
               expect(subject.first.end).to eq(37)
             end
@@ -77,10 +65,10 @@ RSpec.describe ProjectDoc, type: :model do
         end
       end
 
-      context 'when sort is specified' do
+      context 'with sort specified' do
         let(:sort) { true }
 
-        it 'return an array of blocks sorted by begin' do
+        it 'returns blocks sorted by begin' do
           expect(subject.second.hid).to eq('B2')
           expect(subject.third.hid).to eq('B3')
         end
