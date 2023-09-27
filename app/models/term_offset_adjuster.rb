129b6cb32de96e3cba1834340d762fea49457844
ledsun <shigeru.nakajima@gmail.com>
2023-09-27 17:06:58 +0900

    Add argument checks

--- app/models/term_offset_adjuster.rb
+++ app/models/term_offset_adjuster.rb
@@ -4,6 +4,10 @@ class TermOffsetAdjuster
   attr_reader :terms
 
   def initialize(terms, span, context_size)
+    raise ArgumentError, 'terms is nil' if terms.nil?
+    raise ArgumentError, 'span is nil' if span.nil?
+    raise ArgumentError, 'span has begin property' unless span.has_key?(:begin)
+
     @terms = terms
     @span = span
     @offset_size = offset_size(span, context_size) if span.present?
