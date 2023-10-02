0bfcc24ccb8245c5202bb7eee0250a58fc1f1e5e
ledsun <shigeru.nakajima@gmail.com>
2023-10-02 17:32:10 +0900

    Obtains the block that matches the obj specified by term.

--- spec/models/project_doc/annotation_in_spec.rb
+++ spec/models/project_doc/annotation_in_spec.rb
@@ -75,6 +75,18 @@ RSpec.describe ProjectDoc, type: :model do
         it { expect(subject.modifications).to be_empty }
         it { expect(subject.attributes).to be_empty }
       end
+
+      context 'obj of block is matched' do
+        let(:term) { '1st line' }
+
+        it { expect(subject.denotations).to be_empty }
+        it { expect(subject.blocks.count).to eq(1) }
+        it { expect(subject.blocks).to include(block1) }
+        it { expect(subject.relations).to be_empty }
+        it { expect(subject.modifications).to be_empty }
+        it { expect(subject.attributes.count).to eq(1) }
+        it { expect(subject.attributes).to include(attribute3) }
+      end
     end
   end
 end
