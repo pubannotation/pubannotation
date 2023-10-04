d1eafd6d470906646e31e9107cbf670504e954ee
ledsun <shigeru.nakajima@gmail.com>
2023-10-04 14:02:40 +0900

    Refactor annotations_spec.rb

--- spec/requests/annotations_spec.rb
+++ spec/requests/annotations_spec.rb
@@ -2,61 +2,60 @@ require 'rails_helper'
 
 RSpec.describe "Annotations", type: :request do
   describe "GET /docs/sourcedb/:sourcedb/sourceid/:sourceid/spans/:begin-:end/annotations(.:format)" do
-    let(:format_value) { :json }
+    let(:doc) { create(:doc) }
+    let(:base_path) { "/docs/sourcedb/PubMed/sourceid" }
 
-    context 'when document is not found' do
-      before do
-        get "/docs/sourcedb/abc/sourceid/123/annotations.json"
+    describe 'document status' do
+      subject { response }
+
+      context 'when document is not found' do
+        before { get "#{base_path}/123/annotations.json" }
+
+        it { is_expected.to have_http_status(404) }
+        it { is_expected.to match_response_body({ message: 'File not found.' }.to_json) }
       end
 
-      it "returns 404 response" do
-        expect(response).to have_http_status(404)
-      end
+      context 'when document is found' do
+        before { get "#{base_path}/#{doc.sourceid}/annotations.json" }
 
-      it "returns JSON" do
-        expect(response.body).to eq({ message: 'File not found.' }.to_json)
+        it { is_expected.to have_http_status(200) }
+        it { is_expected.to match_response_body(doc_as_json(doc)) }
       end
     end
 
-    context 'when document is found' do
-      let(:doc) { create(:doc) }
+    describe 'partial document annotation' do
+      let(:begin_value) { 1 }
+      let(:end_value) { 2 }
 
       before do
-        get "/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}/annotations.json"
+        get "#{base_path}/#{doc.sourceid}/spans/#{begin_value}-#{end_value}/annotations.json"
       end
 
-      it 'returns 200 response' do
-        expect(response).to have_http_status(200)
-      end
-
-      it 'returns JSON' do
+      it 'returns JSON with annotation of part of document' do
         expect(response.body).to eq({
-                                      target: "http://test.pubannotation.org/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}",
+                                      target: "http://test.pubannotation.org#{base_path}/#{doc.sourceid}",
                                       sourcedb: 'PubMed',
                                       sourceid: doc.sourceid,
-                                      text: "This is a test.\nTest are implemented.\nImplementation is difficult.",
+                                      text: "h",
                                       tracks: []
                                     }.to_json)
       end
+    end
+  end
 
-      context 'begin and end are specified' do
-        let(:begin_value) { 1 }
-        let(:end_value)   { 2 }
+  def doc_as_json(doc)
+    {
+      target: "http://test.pubannotation.org/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}",
+      sourcedb: 'PubMed',
+      sourceid: doc.sourceid,
+      text: "This is a test.\nTest are implemented.\nImplementation is difficult.",
+      tracks: []
+    }.to_json
+  end
 
-        before do
-          get doc_sourcedb_sourceid_path(sourcedb: 'PubMed', sourceid: doc.sourceid, begin: begin_value, end: end_value, format: format_value)
-        end
-
-        it 'returns JSON with annotation of part of document' do
-          expect(response.body).to eq({
-                                        target: "http://test.pubannotation.org/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}",
-                                        sourcedb: 'PubMed',
-                                        sourceid: doc.sourceid,
-                                        text: "h",
-                                        tracks: []
-                                      }.to_json)
-        end
-      end
+  RSpec::Matchers.define :match_response_body do |expected_body|
+    match do |response|
+      response.body == expected_body
     end
   end
 end
