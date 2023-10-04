cef5d1d470a45c0d28c66ceb9d94481f3a779c92
ledsun <shigeru.nakajima@gmail.com>
2023-10-04 13:59:57 +0900

    Add test without begin end

--- spec/requests/annotations_spec.rb
+++ spec/requests/annotations_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Annotations", type: :request do
   describe "GET /docs/sourcedb/:sourcedb/sourceid/:sourceid/spans/:begin-:end/annotations(.:format)" do
-    let(:begin_value) { 1 }
-    let(:end_value)   { 2 }
     let(:format_value) { :json }
 
     context 'when document is not found' do
       before do
-        get doc_sourcedb_sourceid_path(sourcedb: 'abc', sourceid: '123', begin: begin_value, end: end_value, format: format_value)
+        get "/docs/sourcedb/abc/sourceid/123/annotations.json"
       end
 
       it "returns 404 response" do
@@ -24,7 +22,7 @@ RSpec.describe "Annotations", type: :request do
       let(:doc) { create(:doc) }
 
       before do
-        get doc_sourcedb_sourceid_path(sourcedb: 'PubMed', sourceid: doc.sourceid, begin: begin_value, end: end_value, format: format_value)
+        get "/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}/annotations.json"
       end
 
       it 'returns 200 response' do
@@ -36,9 +34,28 @@ RSpec.describe "Annotations", type: :request do
                                       target: "http://test.pubannotation.org/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}",
                                       sourcedb: 'PubMed',
                                       sourceid: doc.sourceid,
-                                      text: "h",
+                                      text: "This is a test.\nTest are implemented.\nImplementation is difficult.",
                                       tracks: []
                                     }.to_json)
+      end
+
+      context 'begin and end are specified' do
+        let(:begin_value) { 1 }
+        let(:end_value)   { 2 }
+
+        before do
+          get doc_sourcedb_sourceid_path(sourcedb: 'PubMed', sourceid: doc.sourceid, begin: begin_value, end: end_value, format: format_value)
+        end
+
+        it 'returns JSON with annotation of part of document' do
+          expect(response.body).to eq({
+                                        target: "http://test.pubannotation.org/docs/sourcedb/PubMed/sourceid/#{doc.sourceid}",
+                                        sourcedb: 'PubMed',
+                                        sourceid: doc.sourceid,
+                                        text: "h",
+                                        tracks: []
+                                      }.to_json)
+        end
       end
     end
   end
