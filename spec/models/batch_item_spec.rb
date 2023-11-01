f682d6d4b1ea8b5fa15dcbaf880f0e479a5126a1
ledsun <shigeru.nakajima@gmail.com>
2023-11-01 18:11:10 +0900

    Refactor test code

--- spec/models/batch_item_spec.rb
+++ spec/models/batch_item_spec.rb
@@ -11,32 +11,32 @@ RSpec.describe BatchItem, type: :model do
   end
 
   describe '#<<' do
-    let(:batch_item) { described_class.new }
+    subject(:batch_item) { described_class.new }
 
-    before do
-      batch_item << AnnotationCollection.new([
-                                               { sourcedb: 'PubMed', sourceid: '001', text: 'text' },
-                                               { sourcedb: 'PubMed', sourceid: '001', text: 'text' },
-                                             ].to_json)
+    context 'when adding multiple annotation collections' do
+      before do
+        batch_item << create_annotation_collection('PubMed', '001', 'text')
+        batch_item << create_annotation_collection('PubMed', '002', 'text')
+        batch_item << create_annotation_collection('PMC', 'A01', 'text')
+      end
 
-      batch_item << AnnotationCollection.new([
-                                               { sourcedb: 'PubMed', sourceid: '002', text: 'text' },
-                                             ].to_json)
+      it 'adds an annotation collection to the transaction' do
+        expect(batch_item.annotation_transaction.length).to eq(3)
+      end
 
-      batch_item << AnnotationCollection.new([
-                                               { sourcedb: 'PMC', sourceid: 'A01', text: 'text' },
-                                             ].to_json)
+      it 'adds a sourcedb and sourceid to the index' do
+        expect(batch_item.sourcedb_sourceids_index).to eq({
+                                                            'PubMed' => Set.new(%w[001 002 A01]),
+                                                            'PMC' => Set.new(%w[001 002 A01])
+                                                          })
+      end
     end
+  end
 
-    it 'adds an annotation collection to the transaction' do
-      expect(batch_item.annotation_transaction.length).to eq(3)
-    end
-
-    it 'adds a sourcedb and sourceid to the index' do
-      expect(batch_item.sourcedb_sourceids_index).to eq({
-                                                          'PubMed' => Set.new(%w[001 002 A01]),
-                                                          'PMC' => Set.new(%w[001 002 A01]),
-                                                        })
-    end
+  # Helper method to DRY up the code
+  def create_annotation_collection(sourcedb, sourceid, text)
+    AnnotationCollection.new([
+                               { sourcedb: sourcedb, sourceid: sourceid, text: text }
+                             ].to_json)
   end
 end
