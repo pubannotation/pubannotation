d932840c50c171d325590573fa738054ffe237a6
ledsun <shigeru.nakajima@gmail.com>
2023-11-02 17:38:41 +0900

    Refactor AnnotationCollection

--- app/models/annotation_collection.rb
+++ app/models/annotation_collection.rb
@@ -2,9 +2,10 @@ class AnnotationCollection
   attr_reader :annotations, :sourcedb_sourceid_index
 
   def initialize(json_string)
-    @annotations = parse json_string
-    set_sourcedb_and_sourceid @annotations
-    validate_and_normalize! @annotations
+    parsed_json = parse json_string
+    validate! parsed_json
+    @annotations = normalize parsed_json
+    @sourcedb_sourceid_index = build_index @annotations
   end
 
   private
@@ -14,20 +15,26 @@ class AnnotationCollection
     Array(JSON.parse(json_string, symbolize_names: true))
   end
 
-  def set_sourcedb_and_sourceid(annotations)
-    annotation = annotations.shift
-    raise ArgumentError, "sourcedb and/or sourceid not specified." unless annotation[:sourcedb].present? && annotation[:sourceid].present?
-    @sourcedb_sourceid_index = DocumentSourceIndex.new(annotation[:sourcedb], [annotation[:sourceid]])
-
+  def validate!(annotations)
     annotations.each do |annotation|
-      raise ArgumentError, "sourcedb and/or sourceid not specified." unless annotation[:sourcedb].present? && annotation[:sourceid].present?
-      @sourcedb_sourceid_index.merge DocumentSourceIndex.new(annotation[:sourcedb], [annotation[:sourceid]])
+      unless annotation[:sourcedb].present? && annotation[:sourceid].present?
+        raise ArgumentError, "sourcedb and/or sourceid not specified."
+      end
     end
   end
 
-  def validate_and_normalize!(annotations)
+  def normalize(annotations)
+    annotations.map { AnnotationUtils.normalize! _1 }
+  end
+
+  def build_index(annotations)
+    annotation = annotations.shift
+    index = DocumentSourceIndex.new(annotation[:sourcedb], [annotation[:sourceid]])
+
     annotations.each do |annotation|
-      AnnotationUtils.normalize!(annotation)
+      index.merge DocumentSourceIndex.new(annotation[:sourcedb], [annotation[:sourceid]])
     end
+
+    index
   end
 end
