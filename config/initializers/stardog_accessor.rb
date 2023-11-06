7ec6ea0d0f758d926a2d9539d882131a01e2934a
jdkim <jindong.kim@gmail.com>
2023-11-07 00:09:06 +0900

    REFACTORED namespace definitions moved to tao_rdf initializer

--- config/initializers/stardog_accessor.rb
+++ config/initializers/stardog_accessor.rb
@@ -9,11 +9,3 @@ sd_password = 'anonymous'
 
 Pubann::Application.config.sd = stardog(sd_url, user: sd_user, password: sd_password, reasoning: :sl)
 Pubann::Application.config.db = sd_database
-Pubann::Application.config.namespaces = {
-	owl: 'http://www.w3.org/2002/07/owl#',
-	rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-	rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-	xsd: 'http://www.w3.org/2001/XMLSchema#',
-	tao: 'http://pubannotation.org/ontology/tao.owl#',
-	prj: 'http://pubannotation.org/projects/'
-}
--- config/initializers/tao_rdf.rb
+++ config/initializers/tao_rdf.rb
@@ -1,6 +1,6 @@
 # for TAO RDF indexing
 # Pubann::Application.config.ep_url = 'http://localhost:5820/'
-Pubann::Application.config.ep_url = 'https://ep.pubannotation.org/PubAnnotation/query'
+Pubann::Application.config.ep_url = 'https://ep.pubannotation.org/sparql'
 Pubann::Application.config.ep_database = 'PubAnnotation'
 Pubann::Application.config.ep_user = 'user'
 Pubann::Application.config.ep_password = 'password'
@@ -8,3 +8,13 @@ Pubann::Application.config.project_indexable_max_docs = 200000
 
 Pubann::Application.config.system_path_rdf = "#{Rails.root}/db/rdf/"
 Pubann::Application.config.project_indexable_max_docs = 200000
+
+Pubann::Application.config.namespaces = {
+	owl: 'http://www.w3.org/2002/07/owl#',
+	rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+	rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+	xsd: 'http://www.w3.org/2001/XMLSchema#',
+	pubann: 'http://pubannotation.org/ontology/pubannotation.owl#'
+	tao: 'http://pubannotation.org/ontology/tao.owl#',
+	prj: 'http://pubannotation.org/projects/'
+}
