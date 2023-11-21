a46eb32460f57af213179d6a823647b5c597d1e3
ledsun <shigeru.nakajima@gmail.com>
2023-11-21 17:01:23 +0900

    Refactor test code of TermSearch::DocController

--- spec/requests/term_search/docs_spec.rb
+++ spec/requests/term_search/docs_spec.rb
@@ -5,13 +5,24 @@ RSpec.describe 'TermSearch::DocsController', type: :request do
     let!(:docs) { create_list(:doc, 3) }
 
     context 'when requesting JSON format' do
-      it 'returns docs in JSON format' do
-        get term_search_docs_path, as: :json
+      before { get term_search_docs_path, as: :json }
 
+      it 'returns http success' do
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json')
+      end
 
+      it 'returns content type as JSON' do
+        expect(response.media_type).to eq('application/json')
+      end
+
+      it 'returns correct number of docs' do
         json_response = JSON.parse(response.body)
+        expect(json_response.size).to eq(docs.size)
+      end
+
+      it 'returns docs in correct format' do
+        json_response = JSON.parse(response.body)
+
         expect(json_response).to be_an_instance_of(Array)
         expect(json_response.length).to eq(docs.length)
         expect(json_response[0]).to eq(docs[0].to_list_hash.stringify_keys)
@@ -19,11 +30,17 @@ RSpec.describe 'TermSearch::DocsController', type: :request do
     end
 
     context 'when requesting TSV format' do
-      it 'returns docs in TSV format' do
-        get "#{term_search_docs_path}.tsv"
+      before { get "#{term_search_docs_path}.tsv" }
 
+      it 'returns http success' do
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('text/tab-separated-values')
+      end
+
+      it 'returns content type as TSV' do
+        expect(response.media_type).to eq('text/tab-separated-values')
+      end
+
+      it 'returns correct format of docs' do
         expect(response.body.split("\n").second).to eq(docs[0].to_list_hash.values.join("\t"))
       end
     end
