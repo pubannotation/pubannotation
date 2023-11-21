813b6ea23d358fdfc3791d5b53462234febc7e92
ledsun <shigeru.nakajima@gmail.com>
2023-11-21 15:39:18 +0900

    Add json and tsv response

--- app/controllers/term_search/docs_controller.rb
+++ app/controllers/term_search/docs_controller.rb
@@ -3,8 +3,22 @@
 module TermSearch
   class DocsController < ApplicationController
     def index
-      @docs = Doc.all
-      render json: @docs
+      docs = Doc.all.map(&:to_list_hash)
+
+      respond_to do |format|
+        format.json do
+          send_data docs.to_json,
+                    filename: 'docs.json',
+                    type: 'application/json',
+                    disposition: 'inline'
+        end
+        format.tsv do
+          send_data Doc.hash_to_tsv(docs),
+                    filename: 'docs.tsv',
+                    type: 'text/tab-separated-values',
+                    disposition: 'inline'
+        end
+      end
     end
   end
 end
