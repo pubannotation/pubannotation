a4c11d56c915ffc4499e68d6cfd0dcd5340263e5
ledsun <shigeru.nakajima@gmail.com>
2023-11-24 15:11:04 +0900

    Add test pattern for terms parameter of TermSearch::DocController

--- spec/requests/term_search/docs_spec.rb
+++ spec/requests/term_search/docs_spec.rb
@@ -72,6 +72,18 @@ RSpec.describe 'TermSearch::DocsController', type: :request do
         expect(json_response.size).to eq(1)
         expect(json_response.first).to eq(doc.to_list_hash.stringify_keys)
       end
+
+      context 'when term of denotation is specified' do
+        let(:doc) { docs.second }
+
+        before { get term_search_docs_path(terms: "subject"), as: :json }
+
+        it 'returns only doc with the term' do
+          json_response = JSON.parse(response.body)
+          expect(json_response.size).to eq(1)
+          expect(json_response.first).to eq(doc.to_list_hash.stringify_keys)
+        end
+      end
     end
   end
 end
