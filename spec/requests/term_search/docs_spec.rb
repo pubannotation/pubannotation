5ebae429d73b6d009188a3d531880697f6575a1e
ledsun <shigeru.nakajima@gmail.com>
2023-12-15 10:06:30 +0900

    I stopped using denotes to narrow down attributes. Fixed the test code as well.

--- spec/requests/term_search/docs_spec.rb
+++ spec/requests/term_search/docs_spec.rb
@@ -106,9 +106,10 @@ RSpec.describe 'TermSearch::DocsController', type: :request do
         context 'when denotes is specified as predicate' do
           before { get term_search_docs_path(terms: "Protein", predicates: "denotes"), as: :json }
 
-          it 'returns empty' do
+          it 'returns denotation with attribute has specified term' do
             json_response = JSON.parse(response.body)
-            expect(json_response.size).to eq(0)
+            expect(json_response.size).to eq(1)
+            expect(json_response.first).to eq(doc.to_list_hash.stringify_keys)
           end
 
           context 'when denotaiton object is specified as term' do
