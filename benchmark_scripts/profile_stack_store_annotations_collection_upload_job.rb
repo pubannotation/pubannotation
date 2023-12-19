4da6ed27a0b5d88654175db5c976b666faa9fcdc
ledsun <shigeru.nakajima@gmail.com>
2023-12-19 10:21:30 +0900

    Prepare temp directory in profile script

--- benchmark_scripts/profile_stack_store_annotations_collection_upload_job.rb
+++ benchmark_scripts/profile_stack_store_annotations_collection_upload_job.rb
@@ -1,6 +1,12 @@
+require 'fileutils'
+
 project = Project.find_by(id: 1)
 filepath = './benchmark_scripts/PMC-7646410.json'
 options = { mode: "replace" }
+
+# prepare tmp directory
+FileUtils.mkdir_p 'tmp/uploads'
+FileUtils.rm_rf 'tmp/uploads/PMC-7646410'
 
 StackProf.run(out: 'tmp/stackprof.dump', raw: true) do
   begin
