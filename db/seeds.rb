ecd08bcae6fd9bdd777befba60d7dc3321abbe19
ledsun <shigeru.nakajima@gmail.com>
2023-12-25 12:04:08 +0900

    Add division in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,20 +31,29 @@ Sequencer.create! name: 'PMC',
 
 doc = Doc.create! sourcedb: 'PMC',
                   sourceid: 'PMC0000001',
-                  body: 'This is a test.'
-
-doc.divisions.create! begin: 0,
-                      end: 4,
-                      label: 'p'
-
-doc.divisions.create! begin: 6,
-                      end: 7,
-                      label: 'i'
-
-doc.divisions.create! begin: 9,
-                      end: 10,
-                      label: 'b'
-
-doc.divisions.create! begin: 12,
-                      end: 15,
-                      label: 'p'
+                  body: <<~BODY
+                        I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.
+                        
+                        Five score years ago, a great American, in whose symbolic shadow we stand today, signed the Emancipation Proclamation. This momentous decree came as a great beacon light of hope to millions of Negro slaves who had been seared in the flames of withering injustice. It came as a joyous daybreak to end the long night of their captivity.
+                        
+                        But one hundred years later, the Negro still is not free. One hundred years later, the life of the Negro is still sadly crippled by the manacles of segregation and the chains of discrimination. One hundred years later, the Negro lives on a lonely island of poverty in the midst of a vast ocean of material prosperity. One hundred years later, the Negro is still languished in the corners of American society and finds himself an exile in his own land. And so we've come here today to dramatize a shameful condition.
+                        BODY
+project = doc.projects.create! name: 'Dream',
+                     user: User.first,
+                     accessibility: 1
+t1 = Denotation.create! doc: doc, project: project, hid: 'T1', begin: 58, end: 65, obj: 'history'
+t2 = Denotation.create! doc: doc, project: project, hid: 'T2', begin: 115, end: 122, obj: 'history'
+t3 = Denotation.create! doc: doc, project: project, hid: 'T3', begin: 169, end: 177, obj: "nation"
+t4 = Denotation.create! doc: doc, project: project, hid: 'T4', begin: 866, end: 874, obj: 'nation'
+t5 = Denotation.create! doc: doc, project: project, hid: 'T5', begin: 5, end: 10, obj: 'emotion'
+t6 = Denotation.create! doc: doc, project: project, hid: 'T6', begin: 332, end: 337, obj: 'Negro'
+t7 = Denotation.create! doc: doc, project: project, hid: 'T7', begin: 508, end: 513, obj: 'Negro'
+t8 = Denotation.create! doc: doc, project: project, hid: 'T8', begin: 574, end: 578, obj: 'Negro'
+t9 = Denotation.create! doc: doc, project: project, hid: 'T9', begin: 698, end: 703, obj: 'Negro'
+t10 = Denotation.create! doc: doc, project: project, hid: 'T10', begin: 822, end: 827, obj: 'Negro'
+Attrivute.create! doc: doc, project: project, hid: 'A1', subj: t5, pred: 'emotion', obj: true
+Attrivute.create! doc: doc, project: project, hid: 'A2', subj: t6, pred: 'role', obj: 'default'
+Attrivute.create! doc: doc, project: project, hid: 'A3', subj: t7, pred: 'role', obj: 'default'
+doc.divisions.create! begin: 0, end: 137, label: 'p'
+doc.divisions.create! begin: 139, end: 473, label: 'p'
+doc.divisions.create! begin: 475, end: 990, label: 'p'
