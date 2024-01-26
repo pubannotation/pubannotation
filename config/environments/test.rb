2f5c0bf70c22efffcb1336e3388e6e54839b15c8
ledsun <shigeru.nakajima@gmail.com>
2024-01-26 13:56:32 +0900

    Set default_url_options for test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.application.routes.default_url_options[:host]= 'test.pubannotation.org'
 end
