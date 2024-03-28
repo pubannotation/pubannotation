11957144d1e6c79c61dddfa48ca48445b695cc79
ledsun <shigeru.nakajima@gmail.com>
2024-03-28 13:38:20 +0900

    Fix encoder to serialize

--- app/models/editor.rb
+++ app/models/editor.rb
@@ -9,7 +9,7 @@ class Editor < ActiveRecord::Base
 
 	validates :url, :presence => true
 
-	serialize :parameters, coder: YAML
+	serialize :parameters, coder: JSON
 
 	scope :accessibles, -> (current_user) {
 		if current_user.present?
