f57a985688dfbc4cb799e97ad8c144128ad5a454
yush-nh <yushin.nihei@luxiar.com>
2024-07-26 17:26:06 +0900

    Moved annotation process to model

--- app/controllers/annotation_reception_controller.rb
+++ app/controllers/annotation_reception_controller.rb
@@ -10,13 +10,7 @@ class AnnotationReceptionController < ApplicationController
 
     annotations_col = get_result_from_json_body
 
-    annotations_col.each_with_index do |annotations, i|
-      raise RuntimeError, "annotation result is not a valid JSON object." unless annotations.class == Hash
-      AnnotationUtils.normalize!(annotations)
-      annotator.annotations_transform!(annotations)
-    end
-
-    StoreAnnotationsCollection.new(project, annotations_col, options).call.join
+    annotation_reception.process_annotation(annotations_col, annotator, project, options)
 
     respond_to do |format|
       format.any {head :no_content}
--- app/models/annotation_reception.rb
+++ app/models/annotation_reception.rb
@@ -2,4 +2,14 @@ class AnnotationReception < ApplicationRecord
   validates :annotator_id, presence: true
   validates :project_id, presence: true
   validates :uuid, presence: true
+
+  def process_annotation(annotations_col, annotator, project, options)
+    annotations_col.each_with_index do |annotations, i|
+      raise RuntimeError, "annotation result is not a valid JSON object." unless annotations.class == Hash
+      AnnotationUtils.normalize!(annotations)
+      annotator.annotations_transform!(annotations)
+    end
+
+    StoreAnnotationsCollection.new(project, annotations_col, options).call.join
+  end
 end
