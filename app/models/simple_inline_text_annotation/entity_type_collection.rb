13114869576c36cfc00028d7619ef4fd11be422a
yush-nh <yushin.nihei@luxiar.com>
2024-12-19 09:26:00 +0900

    Changed nested class definition to use ::

--- app/models/simple_inline_text_annotation/entity_type_collection.rb
+++ app/models/simple_inline_text_annotation/entity_type_collection.rb
@@ -1,60 +1,58 @@
-class SimpleInlineTextAnnotation
-  class EntityTypeCollection
-    # ENTITY_TYPE_PATTERN matches a pair of square brackets which is followed by a colon and URL.
-    # Example: [Label]: URL
-    ENTITY_TYPE_PATTERN = /^\[([^\]]+)\]:\s+(.*)/
+class SimpleInlineTextAnnotation::EntityTypeCollection
+  # ENTITY_TYPE_PATTERN matches a pair of square brackets which is followed by a colon and URL.
+  # Example: [Label]: URL
+  ENTITY_TYPE_PATTERN = /^\[([^\]]+)\]:\s+(.*)/
 
-    def initialize(source)
-      @source = source
+  def initialize(source)
+    @source = source
+  end
+
+  def get(label)
+    entity_types[label]
+  end
+
+  # to_config returns a Array of hashes of each entity type.
+  # Example:
+  #   [
+  #     {id: "https://example.com/Person", label: "Person"},
+  #     {id: "https://example.com/Organization", label: "Organization"}
+  #   ]
+  def to_config
+    entity_types.map do |label, id|
+      { id: id, label: label }
     end
+  end
 
-    def get(label)
-      entity_types[label]
-    end
+  def any?
+    entity_types.any?
+  end
 
-    # to_config returns a Array of hashes of each entity type.
-    # Example:
-    #   [
-    #     {id: "https://example.com/Person", label: "Person"},
-    #     {id: "https://example.com/Organization", label: "Organization"}
-    #   ]
-    def to_config
-      entity_types.map do |label, id|
-        { id: id, label: label }
-      end
-    end
+  private
 
-    def any?
-      entity_types.any?
-    end
+  # entity_types returns a Hash structured with label as key and id as value.
+  # Example:
+  #   {
+  #     "Person": "https://example.com/Person",
+  #     "Organization": "https://example.com/Organization"
+  #   }
+  def entity_types
+    @entity_types ||= read_entities_from_source
+  end
 
-    private
+  def read_entities_from_source
+    entity_types = {}
 
-    # entity_types returns a Hash structured with label as key and id as value.
-    # Example:
-    #   {
-    #     "Person": "https://example.com/Person",
-    #     "Organization": "https://example.com/Organization"
-    #   }
-    def entity_types
-      @entity_types ||= read_entities_from_source
-    end
+    @source.scan(SimpleInlineTextAnnotation::ENTITY_TYPE_BLOCK_PATTERN).each do |block|
+      block[0].each_line do |line|
+        match = line.strip.match(ENTITY_TYPE_PATTERN)
 
-    def read_entities_from_source
-      entity_types = {}
-
-      @source.scan(ENTITY_TYPE_BLOCK_PATTERN).each do |block|
-        block[0].each_line do |line|
-          match = line.strip.match(ENTITY_TYPE_PATTERN)
-
-          if match
-            label, id = match[1], match[2]
-            entity_types[label] ||= id
-          end
+        if match
+          label, id = match[1], match[2]
+          entity_types[label] ||= id
         end
       end
-
-      entity_types
     end
+
+    entity_types
   end
 end
--- app/models/simple_inline_text_annotation/parser.rb
+++ app/models/simple_inline_text_annotation/parser.rb
@@ -1,71 +1,69 @@
-class SimpleInlineTextAnnotation
-  class Parser
-    # DENOTATION_PATTERN matches two consecutive pairs of square brackets.
-    # Example: [Annotated Text][Label]
-    DENOTATION_PATTERN = /(?<!\\)\[([^\[]+?)\]\[([^\]]+?)\]/
+class SimpleInlineTextAnnotation::Parser
+  # DENOTATION_PATTERN matches two consecutive pairs of square brackets.
+  # Example: [Annotated Text][Label]
+  DENOTATION_PATTERN = /(?<!\\)\[([^\[]+?)\]\[([^\]]+?)\]/
 
-    # ESCAPE_PATTERN matches a backslash (\) preceding two consecutive pairs of square brackets.
-    # Example: \[This is a part of][original text]
-    ESCAPE_PATTERN = /\\(?=\[[^\]]+\]\[[^\]]+\])/
+  # ESCAPE_PATTERN matches a backslash (\) preceding two consecutive pairs of square brackets.
+  # Example: \[This is a part of][original text]
+  ESCAPE_PATTERN = /\\(?=\[[^\]]+\]\[[^\]]+\])/
 
-    def initialize(source)
-      @source = source.dup.freeze
-      @denotations = []
-      @entity_type_collection = SimpleInlineTextAnnotation::EntityTypeCollection.new(source)
+  def initialize(source)
+    @source = source.dup.freeze
+    @denotations = []
+    @entity_type_collection = SimpleInlineTextAnnotation::EntityTypeCollection.new(source)
+  end
+
+  def parse
+    full_text = source_without_references
+
+    while full_text =~ DENOTATION_PATTERN
+      match = Regexp.last_match
+      target_text = match[1]
+      label = match[2]
+
+      begin_pos = match.begin(0)
+      end_pos = begin_pos + target_text.length - 1 # -1 to adapt with zero-based indexing.
+      obj = get_obj_for(label)
+
+      @denotations << SimpleInlineTextAnnotation::Denotation.new(begin_pos, end_pos, obj)
+
+      # Replace the processed annotation with its text content
+      full_text[match.begin(0)...match.end(0)] = target_text
     end
 
-    def parse
-      full_text = source_without_references
+    SimpleInlineTextAnnotation.new(
+      text_without_escape_backslash(full_text),
+      @denotations.map(&:to_h),
+      config
+    ).to_h
+  end
 
-      while full_text =~ DENOTATION_PATTERN
-        match = Regexp.last_match
-        target_text = match[1]
-        label = match[2]
+  private
 
-        begin_pos = match.begin(0)
-        end_pos = begin_pos + target_text.length - 1 # -1 to adapt with zero-based indexing.
-        obj = get_obj_for(label)
+  def source_without_references
+    # Remove references from the source.
 
-        @denotations << SimpleInlineTextAnnotation::Denotation.new(begin_pos, end_pos, obj)
+    @source.gsub(SimpleInlineTextAnnotation::ENTITY_TYPE_BLOCK_PATTERN) do |block|
+      block.start_with?("\n\n") ? "\n\n" : ""
+    end.strip
+  end
 
-        # Replace the processed annotation with its text content
-        full_text[match.begin(0)...match.end(0)] = target_text
-      end
+  def get_obj_for(label)
+    @entity_type_collection.get(label) || label
+  end
 
-      SimpleInlineTextAnnotation.new(
-        text_without_escape_backslash(full_text),
-        @denotations.map(&:to_h),
-        config
-      ).to_h
-    end
+  def text_without_escape_backslash(text)
+    # Remove backslashes used to escape inline annotation format.
+    # For example, `\[Elon Musk][Person]` is treated as plain text
+    # rather than an annotation. This method removes the leading
+    # backslash and keeps the text as `[Elon Musk][Person]`.
 
-    private
+    text.gsub(ESCAPE_PATTERN, '')
+  end
 
-    def source_without_references
-      # Remove references from the source.
+  def config
+    return nil unless @entity_type_collection.any?
 
-      @source.gsub(ENTITY_TYPE_BLOCK_PATTERN) do |block|
-        block.start_with?("\n\n") ? "\n\n" : ""
-      end.strip
-    end
-
-    def get_obj_for(label)
-      @entity_type_collection.get(label) || label
-    end
-
-    def text_without_escape_backslash(text)
-      # Remove backslashes used to escape inline annotation format.
-      # For example, `\[Elon Musk][Person]` is treated as plain text
-      # rather than an annotation. This method removes the leading
-      # backslash and keeps the text as `[Elon Musk][Person]`.
-
-      text.gsub(ESCAPE_PATTERN, '')
-    end
-
-    def config
-      return nil unless @entity_type_collection.any?
-
-      { "entity types": @entity_type_collection.to_config }
-    end
+    { "entity types": @entity_type_collection.to_config }
   end
 end
