a9b6cdba9a3cccd607e926ec3c67cac7eb2bdac6
yush-nh <yushin.nihei@luxiar.com>
2024-12-19 14:51:29 +0900

    Changed class definition from :: to nested

--- app/models/simple_inline_text_annotation/denotation.rb
+++ app/models/simple_inline_text_annotation/denotation.rb
@@ -1,10 +1,12 @@
-class SimpleInlineTextAnnotation::Denotation
-  def initialize(begin_pos, end_pos, obj)
-    @span = { begin: begin_pos, end: end_pos }
-    @obj = obj
-  end
+class SimpleInlineTextAnnotation
+  class Denotation
+    def initialize(begin_pos, end_pos, obj)
+      @span = { begin: begin_pos, end: end_pos }
+      @obj = obj
+    end
 
-  def to_h
-    { span: @span, obj: @obj }
+    def to_h
+      { span: @span, obj: @obj }
+    end
   end
 end
--- app/models/simple_inline_text_annotation/parser.rb
+++ app/models/simple_inline_text_annotation/parser.rb
@@ -1,50 +1,52 @@
-class SimpleInlineTextAnnotation::Parser
-  # DENOTATION_PATTERN matches two consecutive pairs of square brackets.
-  # Example: [Annotated Text][Label]
-  DENOTATION_PATTERN = /(?<!\\)\[([^\[]+?)\]\[([^\]]+?)\]/
+class SimpleInlineTextAnnotation
+  class Parser
+    # DENOTATION_PATTERN matches two consecutive pairs of square brackets.
+    # Example: [Annotated Text][Label]
+    DENOTATION_PATTERN = /(?<!\\)\[([^\[]+?)\]\[([^\]]+?)\]/
 
-  def initialize(source)
-    @source = source.dup.freeze
-    @denotations = []
-    @entity_type_collection = SimpleInlineTextAnnotation::EntityTypeCollection.new(source)
-  end
-
-  def parse
-    full_text = source_without_references
-
-    while full_text =~ DENOTATION_PATTERN
-      match = Regexp.last_match
-      target_text = match[1]
-      label = match[2]
-
-      begin_pos = match.begin(0)
-      end_pos = begin_pos + target_text.length - 1 # -1 to adapt with zero-based indexing.
-      obj = get_obj_for(label)
-
-      @denotations << SimpleInlineTextAnnotation::Denotation.new(begin_pos, end_pos, obj)
-
-      # Replace the processed annotation with its text content
-      full_text[match.begin(0)...match.end(0)] = target_text
+    def initialize(source)
+      @source = source.dup.freeze
+      @denotations = []
+      @entity_type_collection = EntityTypeCollection.new(source)
     end
 
-    SimpleInlineTextAnnotation.new(
-      full_text,
-      @denotations,
-      @entity_type_collection
-    )
-  end
+    def parse
+      full_text = source_without_references
 
-  private
+      while full_text =~ DENOTATION_PATTERN
+        match = Regexp.last_match
+        target_text = match[1]
+        label = match[2]
 
-  def source_without_references
-    # Remove references from the source.
+        begin_pos = match.begin(0)
+        end_pos = begin_pos + target_text.length - 1 # -1 to adapt with zero-based indexing.
+        obj = get_obj_for(label)
 
-    @source.gsub(SimpleInlineTextAnnotation::ENTITY_TYPE_BLOCK_PATTERN) do |block|
-      block.start_with?("\n\n") ? "\n\n" : ""
-    end.strip
-  end
+        @denotations << Denotation.new(begin_pos, end_pos, obj)
 
-  def get_obj_for(label)
-    @entity_type_collection.get(label) || label
+        # Replace the processed annotation with its text content
+        full_text[match.begin(0)...match.end(0)] = target_text
+      end
+
+      SimpleInlineTextAnnotation.new(
+        full_text,
+        @denotations,
+        @entity_type_collection
+      )
+    end
+
+    private
+
+    def source_without_references
+      # Remove references from the source.
+
+      @source.gsub(ENTITY_TYPE_BLOCK_PATTERN) do |block|
+        block.start_with?("\n\n") ? "\n\n" : ""
+      end.strip
+    end
+
+    def get_obj_for(label)
+      @entity_type_collection.get(label) || label
+    end
   end
 end
