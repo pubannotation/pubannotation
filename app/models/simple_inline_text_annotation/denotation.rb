f72310ab8c33a27017e3ddb58dbd5642f095e625
yush-nh <yushin.nihei@luxiar.com>
2025-01-07 16:06:52 +0900

    Cleaned denotation class

--- app/models/simple_inline_text_annotation/denotation.rb
+++ app/models/simple_inline_text_annotation/denotation.rb
@@ -1,31 +1,28 @@
 class SimpleInlineTextAnnotation
   class Denotation
-    attr_reader :span, :obj
+    attr_reader :begin_pos, :end_pos, :obj
 
     def initialize(begin_pos, end_pos, obj)
-      @span = { begin: begin_pos, end: end_pos }
+      @begin_pos = begin_pos
+      @end_pos = end_pos
       @obj = obj
     end
 
-    def begin_pos
-      @span[:begin]
-    end
-
-    def end_pos
-      @span[:end]
+    def span
+      { begin: @begin_pos, end: @end_pos }
     end
 
     def to_h
-      { span: @span, obj: @obj }
+      { span: span, obj: @obj }
     end
 
     def nested_within?(other)
-      other.begin_pos <= begin_pos && end_pos <= other.end_pos
+      other.begin_pos <= @begin_pos && @end_pos <= other.end_pos
     end
 
     def boundary_crossing?(other)
-      starts_inside_other = begin_pos > other.begin_pos && begin_pos < other.end_pos
-      ends_inside_other = end_pos > other.begin_pos && end_pos < other.end_pos
+      starts_inside_other = @begin_pos > other.begin_pos && @begin_pos < other.end_pos
+      ends_inside_other = @end_pos > other.begin_pos && @end_pos < other.end_pos
 
       starts_inside_other || ends_inside_other
     end
