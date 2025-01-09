0927867ca288aa4f5e78fdb1c5ec6a27f444c6ab
yush-nh <yushin.nihei@luxiar.com>
2025-01-09 10:12:43 +0900

    Add entity_types property for readability

--- app/models/simple_inline_text_annotation/generator.rb
+++ app/models/simple_inline_text_annotation/generator.rb
@@ -5,15 +5,15 @@ class SimpleInlineTextAnnotation
     def initialize(source)
       @source = source.dup.freeze
       @denotations = build_denotations(source[:denotations] || [])
+      @config = @source[:config]
     end
 
     def generate
       text = @source[:text]
       denotations = validate(@denotations)
-      config = @source[:config]
 
-      annotated_text = annotate_text(text, denotations, config)
-      label_definitions = build_label_definitions(config)
+      annotated_text = annotate_text(text, denotations)
+      label_definitions = build_label_definitions
 
       [annotated_text, label_definitions].compact.join("\n\n")
     end
@@ -24,12 +24,12 @@ class SimpleInlineTextAnnotation
       denotations.map { |d| Denotation.new(d[:span][:begin], d[:span][:end], d[:obj])}
     end
 
-    def annotate_text(text, denotations, config)
+    def annotate_text(text, denotations)
       # Annotate text from the end to ensure position calculation.
       denotations.sort_by(&:begin_pos).reverse_each do |denotation|
         begin_pos = denotation.begin_pos
         end_pos = denotation.end_pos
-        obj = get_obj(denotation.obj, config)
+        obj = get_obj(denotation.obj)
 
         annotated_text = "[#{text[begin_pos...end_pos]}][#{obj}]"
         text = text[0...begin_pos] + annotated_text + text[end_pos..]
@@ -38,17 +38,21 @@ class SimpleInlineTextAnnotation
       text
     end
 
-    def get_obj(obj, config)
-      return obj unless config && config[:"entity types"]
+    def entity_types
+      @config ? @config[:"entity types"] : nil
+    end
 
-      entity = config[:"entity types"].find { |entity_type| entity_type[:id] == obj }
+    def get_obj(obj)
+      return obj unless entity_types
+
+      entity = entity_types.find { |entity_type| entity_type[:id] == obj }
       entity ? entity[:label] : obj
     end
 
-    def build_label_definitions(config)
-      return nil unless config && config[:"entity types"]
+    def build_label_definitions
+      return nil unless entity_types
 
-      config[:"entity types"].map do |entity|
+      entity_types.map do |entity|
         "[#{entity[:label]}]: #{entity[:id]}"
       end.join("\n")
     end
