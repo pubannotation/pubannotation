d3b252e8913912816cc68526c5f69c37d29bdd6f
yush-nh <yushin.nihei@luxiar.com>
2025-01-10 17:34:08 +0900

    Add request/model tests

---
+++ spec/models/textae_annotation_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe TextaeAnnotation, type: :model do
+  describe '.generate_textae_html' do
+    it 'generates textae HTML with annotation' do
+      annotation = 'sample annotation'
+      html = TextaeAnnotation.generate_textae_html(annotation)
+
+      expect(html).to include(annotation)
+    end
+  end
+
+  describe '#clean_old_annotations' do
+    context 'when creating new instance' do
+      it 'should delete old annotations' do
+        old_annotation = TextaeAnnotation.create(annotation: '{ "text": "hello" }', created_at: 2.days.ago)
+        TextaeAnnotation.create(annotation: '{ "text": "hello" }')
+
+        expect(TextaeAnnotation.count).to eq(1)
+        expect(TextaeAnnotation.find_by(id: old_annotation.id)).to be_nil
+      end
+    end
+  end
+end
