e74925416b0d8a18e15388bac59afa95e79a97cf
yush-nh <yushin.nihei@luxiar.com>
2025-01-10 11:49:48 +0900

    Implement create action in TextaeController

--- app/controllers/textae_controller.rb
+++ app/controllers/textae_controller.rb
@@ -2,8 +2,37 @@ class TextaeController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[create show]
 
   def create
+    unless ['text/markdown', 'application/json'].include?(request.content_type)
+      render json: { error: 'Invalid content-type. Please set the correct content-type according to the request.' }, status: :unsupported_media_type
+      return
+    end
+
+    annotation = parse_request_body
+    textae_annotation = TextaeAnnotation.create!(annotation: annotation)
+
+    TextaeAnnotation.older_than_one_day.destroy_all
+
+    render json: {
+      message: 'Request was successfully processed. To see the generated textae html, send GET request to result_url.',
+      result_url: "#{Rails.application.config.host_url}/textae/#{textae_annotation.uuid}"
+    }, status: :created
+  rescue => e
+    render json: { error: e.message }, status: :internal_server_error
   end
 
   def show
+  end
+
+  private
+
+  def parse_request_body
+    body = request.body.read
+
+    if request.content_type == 'text/markdown'
+      annotation = SimpleInlineTextAnnotation.parse(body)
+      JSON.pretty_generate(annotation)
+    elsif request.content_type == 'application/json'
+      body
+    end
   end
 end
--- app/models/textae_annotation.rb
+++ app/models/textae_annotation.rb
@@ -1,2 +1,3 @@
 class TextaeAnnotation < ApplicationRecord
+  scope :older_than_one_day, -> { where("created_at < ?", 1.day.ago) }
 end
