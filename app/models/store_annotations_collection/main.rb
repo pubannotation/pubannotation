7229d186981df7ee795d3da659b54081e0d486da
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-06-23 15:28:46 +0900

    Add AlignedResults#save method

--- app/models/store_annotations_collection/main.rb
+++ app/models/store_annotations_collection/main.rb
@@ -25,39 +25,8 @@ module StoreAnnotationsCollection
         # We are creating our own threads that Rails do not manage.
         # Explicitly releases the connection to the DB.
         ActiveRecord::Base.connection_pool.with_connection do
-          @warnings.concat save(result, @options, @project)
+          @warnings.concat result.save(@options, @project)
           @warnings.finalize
-        end
-      end
-    end
-
-    private
-
-    def save(result, options, project)
-      result.annotations_for_doc_collection.each do |annotations_for_doc|
-        pretreatment_according_to options,
-                                  project,
-                                  annotations_for_doc.doc,
-                                  annotations_for_doc.annotations
-      end
-
-      warning_messages, valid_annotations = result.valid_annotations
-      InstantiateAndSaveAnnotationsCollection.call(project, valid_annotations) if valid_annotations.present?
-
-      warning_messages
-    end
-
-    def pretreatment_according_to(options, project, document, annotations)
-      if options[:mode] == 'replace'
-        project.delete_doc_annotations document
-      else
-        case options[:mode]
-        when 'add'
-          annotations.each { |a| project.reid_annotations!(a, document) }
-        when 'merge'
-          annotations.each { |a| project.reid_annotations!(a, document) }
-          base_annotations = document.hannotations(project, nil, nil)
-          annotations.each { |a| AnnotationUtils.prepare_annotations_for_merging!(a, base_annotations) }
         end
       end
     end
--- app/models/text_align/aligned_results.rb
+++ app/models/text_align/aligned_results.rb
@@ -9,6 +9,37 @@ module TextAlign
       @warnings = warnings
     end
 
+    def save(options, project)
+      @annotations_for_doc_collection.each do |annotations_for_doc|
+        pretreatment_according_to options,
+                                  project,
+                                  annotations_for_doc.doc,
+                                  annotations_for_doc.annotations
+      end
+
+      warning_messages, valid_annotations = self.valid_annotations
+      StoreAnnotationsCollection::InstantiateAndSaveAnnotationsCollection.call(project, valid_annotations) if valid_annotations.present?
+
+      warning_messages
+    end
+
+    private
+
+    def pretreatment_according_to(options, project, document, annotations)
+      if options[:mode] == 'replace'
+        project.delete_doc_annotations document
+      else
+        case options[:mode]
+        when 'add'
+          annotations.each { |a| project.reid_annotations!(a, document) }
+        when 'merge'
+          annotations.each { |a| project.reid_annotations!(a, document) }
+          base_annotations = document.hannotations(project, nil, nil)
+          annotations.each { |a| AnnotationUtils.prepare_annotations_for_merging!(a, base_annotations) }
+        end
+      end
+    end
+
     def valid_annotations
       warnings = []
       results = @annotations_for_doc_collection.reduce([]) do |results, annotations_for_doc|
