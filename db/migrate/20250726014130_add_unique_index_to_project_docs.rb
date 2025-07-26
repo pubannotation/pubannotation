b9b21bad3785a8a94a73dc7fd44bf46b47f61264
jdkim <jindong.kim@gmail.com>
2025-07-26 11:02:05 +0900

    FIXED to check existence of the index before removal

--- db/migrate/20250726014130_add_unique_index_to_project_docs.rb
+++ db/migrate/20250726014130_add_unique_index_to_project_docs.rb
@@ -1,11 +1,14 @@
 class AddUniqueIndexToProjectDocs < ActiveRecord::Migration[8.0]
+  INDEX_NAME = "index_project_docs_on_project_id_and_doc_id"
+
   def up
-    remove_index :project_docs, name: "index_project_docs_on_project_id_and_doc_id"
+    if index_exists?(:project_docs, [:project_id, :doc_id], unique: false, name: INDEX_NAME)
+      remove_index :project_docs, name: INDEX_NAME
+    end
     add_index :project_docs, [:project_id, :doc_id], unique: true, name: "index_project_docs_on_project_id_and_doc_id"
   end
 
   def down
     remove_index :project_docs, name: "index_project_docs_on_project_id_and_doc_id"
-    add_index :project_docs, [:project_id, :doc_id], name: "index_project_docs_on_project_id_and_doc_id"
   end
 end
