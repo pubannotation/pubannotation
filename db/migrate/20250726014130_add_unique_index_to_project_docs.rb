4121ffeb4f343c58646b2c1348ccddc2fe29e4ec
xaiBUh29wX <arino.tamada@luxiar.com>
2026-06-03 17:43:27 +0900

    Fix migration to detect unique index in AddUniqueIndexToProjectDocs

--- db/migrate/20250726014130_add_unique_index_to_project_docs.rb
+++ db/migrate/20250726014130_add_unique_index_to_project_docs.rb
@@ -2,7 +2,7 @@ class AddUniqueIndexToProjectDocs < ActiveRecord::Migration[8.0]
   INDEX_NAME = "index_project_docs_on_project_id_and_doc_id"
 
   def up
-    if index_exists?(:project_docs, [:project_id, :doc_id], unique: false, name: INDEX_NAME)
+    if index_exists?(:project_docs, [:project_id, :doc_id], name: INDEX_NAME)
       remove_index :project_docs, name: INDEX_NAME
     end
     add_index :project_docs, [:project_id, :doc_id], unique: true, name: "index_project_docs_on_project_id_and_doc_id"
