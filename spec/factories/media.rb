c795677ee9135939288979e8d9bcbe41f776676b
xaiBUh29wX <arino.tamada@luxiar.com>
2026-06-15 15:40:36 +0900

    Add Medium model spec

---
+++ spec/factories/media.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :medium do
+    sequence(:sourcedb) { |n| "MediaDB#{n}" }
+    sequence(:sourceid) { |n| "media_#{n}" }
+    media_type { :image }
+    content_type { 'image/jpeg' }
+  end
+end
