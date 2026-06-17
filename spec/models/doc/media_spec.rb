b98bf7579d5aa76924baa89b32d27e1e4bf96f92
xaiBUh29wX <arino.tamada@luxiar.com>
2026-06-17 10:43:17 +0900

    Refactor medium spec with subject and context blocks

--- spec/models/doc/media_spec.rb
+++ spec/models/doc/media_spec.rb
@@ -3,33 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Doc, type: :model do
-  let(:doc) { create(:doc, medium: medium) }
+  subject(:doc) { create(:doc, medium: medium) }
 
-  describe 'medium association' do
-    context 'with medium' do
-      let(:medium) { create(:medium) }
+  context 'with medium' do
+    let(:medium) { create(:medium) }
 
-      it 'returns the associated Medium via belongs_to' do
-        expect(doc.medium).to eq(medium)
-      end
+    describe 'medium association' do
+      it { is_expected.to have_attributes(medium: medium) }
     end
 
-    context 'without medium' do
-      let(:medium) { nil }
+    describe 'medium_id immutability' do
+      it 'cannot change medium after creation' do
+        other_medium = create(:medium, sourcedb: 'DB2', sourceid: 'id2')
+        doc.medium = other_medium
 
-      it 'medium is nil' do
-        expect(doc.medium).to be_nil
+        expect(doc).not_to be_valid
+        expect(doc.errors[:base]).to include('Media reference cannot be changed after creation')
       end
     end
   end
 
-  describe 'medium_id immutability' do
-    context 'with medium' do
-      let(:medium) { create(:medium, sourcedb: 'DB1', sourceid: 'id1') }
+  context 'without medium' do
+    let(:medium) { nil }
 
-      it 'cannot change medium after creation' do
-        other_medium = create(:medium, sourcedb: 'DB2', sourceid: 'id2')
-        doc.medium = other_medium
+    describe 'medium association' do
+      it { is_expected.to have_attributes(medium: nil) }
+    end
+
+    describe 'medium_id immutability' do
+      it 'cannot add medium after creation' do
+        new_medium = create(:medium)
+        doc.medium = new_medium
+
         expect(doc).not_to be_valid
         expect(doc.errors[:base]).to include('Media reference cannot be changed after creation')
       end
