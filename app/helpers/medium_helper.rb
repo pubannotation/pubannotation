6513dc75ab6937b3ac466a6ba35e2a5dac563923
xaiBUh29wX <arino.tamada@luxiar.com>
2026-07-06 11:47:08 +0900

    Add medium_tag helper to render video/audio playback for media

--- app/helpers/medium_helper.rb
+++ app/helpers/medium_helper.rb
@@ -2,4 +2,15 @@ module MediumHelper
   def current_user_owns_medium?(medium)
     medium.user == current_user
   end
+
+  def medium_tag(medium, **options)
+    return unless medium.file.attached?
+
+    url = url_for(medium.file)
+    case medium.media_type
+    when 'image' then image_tag(url, **options)
+    when 'video' then video_tag(url, controls: true, **options)
+    when 'audio' then audio_tag(url, controls: true, **options)
+    end
+  end
 end
